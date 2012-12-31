8b18f23f67f16063c20fa26d116dece092664ef6
John Horback <jhorback@gmail.com>
2012-12-31 11:51:15 -0500

    New view for change home page

---
+++ Harbor.UI/Scripts/Apps/Settings/ChangeHomePageView.js
@@ -0,0 +1,19 @@
+﻿Settings.ChangeHomePageView = Application.View.extend({
+	initialize: function () {
+		Settings.regions.main.hideEl();
+	},
+	
+	events: {
+		"click [data-rel=cancel]": function () {
+			this.remove();
+			Settings.regions.main.showEl();
+		}
+	},
+
+	render: function () {
+		
+		this.template("Settings-ChangeHomePage", this.$el)();
+		
+
+	}
+});
