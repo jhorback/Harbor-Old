9cd92f4ba11ed3136e8dffb2e7e5591f2eba4e40
John Horback <jhorback@gmail.com>
2013-01-30 18:15:53 -0500

    split out classes

---
+++ Harbor.UI/Models/JSPM/TemplateAction.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Harbor.UI.Models.JSPM
+{
+	public class TemplateAction
+	{
+		public TemplateAction() { }
+
+		public TemplateAction(string action, string controller)
+		{
+			Action = action;
+			Controller = controller;
+		}
+
+		public TemplateAction(string action, string controller, string routeValues)
+		{
+			Action = action;
+			Controller = controller;
+			RouteValues = RouteValues;
+		}
+
+		public string Action { get; set; }
+		public string Controller { get; set; }
+		public object RouteValues { get; set; }
+	}
+}
