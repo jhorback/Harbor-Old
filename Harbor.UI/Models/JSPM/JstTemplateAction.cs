b0580fdb79371e68c8b03d3b9c828d211322f8b1
John Horback <jhorback@gmail.com>
2013-01-30 18:16:05 -0500

    split out classes

---
+++ Harbor.UI/Models/JSPM/JstTemplateAction.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Harbor.UI.Models.JSPM
+{
+	public class JstTemplateAction : TemplateAction
+	{
+		/// <summary>
+		/// Creates a TemplateAction for the HomeController JST action
+		/// passing the viewpath relative to the views folder. (no need for the .cshtml extension).
+		/// </summary>
+		/// <param name="viewpath"></param>
+		public JstTemplateAction(string viewpath) : base ("jst", "home")
+		{
+			this.RouteValues = new
+			    {
+					viewpath = viewpath
+			    };
+		}
+	}
+}
--- Harbor.UI/Models/JSPM/PackageTable.cs
+++ Harbor.UI/Models/JSPM/PackageTable.cs
@@ -1,5 +1,4 @@
 ﻿
-using System.Web.Optimization;
 
 namespace Harbor.UI.Models.JSPM
 {
@@ -11,46 +10,5 @@ namespace Harbor.UI.Models.JSPM
 		}
 
 		public static PackageCollection Packages { get; private set; }
-	}
-
-
-
-	public class TemplateAction
-	{
-		public TemplateAction() { }
-
-		public TemplateAction(string action, string controller)
-		{
-			Action = action;
-			Controller = controller;
-		}
-
-		public TemplateAction(string action, string controller, string routeValues)
-		{
-			Action = action;
-			Controller = controller;
-			RouteValues = RouteValues;
-		}
-
-		public string Action { get; set; }
-		public string Controller { get; set; }
-		public object RouteValues { get; set; }
-	}
-
-
-	public class JstTemplateAction : TemplateAction
-	{
-		/// <summary>
-		/// Creates a TemplateAction for the HomeController JST action
-		/// passing the viewpath relative to the views folder. (no need for the .cshtml extension).
-		/// </summary>
-		/// <param name="viewpath"></param>
-		public JstTemplateAction(string viewpath) : base ("jst", "home")
-		{
-			this.RouteValues = new
-			    {
-					viewpath = viewpath
-			    };
-		}
 	}
 }
