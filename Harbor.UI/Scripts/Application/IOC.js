e6c7aea702c1f828b9e486721b1a41654cb936e6
John Horback <jhorback@gmail.com>
2013-05-30 16:28:17 -0400

    ioc adjustment

--- Harbor.UI/Scripts/Application/IOC.js
+++ Harbor.UI/Scripts/Application/IOC.js
@@ -17,15 +17,15 @@ var IOC;
 (function () {
 	var ioc = {
 		registry: {},
-		
+
 		getFnArgs: function (fn) {
 			var val = fn.toString().match(/^function\s*[^\(]*\(\s*([^\)]*)\)/m)[1].split(',');
 			return val[0] === "" ? [] : val;
 		},
-		
-		get: function (name, request) {
+
+		get: function (name, request, args) {
 			var t, deps, resolved, reg, instance,
-				injectedInstance, returnValue;
+			    injectedInstance, returnValue, len;
 
 			name = name.replace(/^\s+|\s+$/g, ''); // trim
 			reg = request.container[name];
@@ -42,10 +42,14 @@ var IOC;
 				return reg.instance;
 			}
 
-			resolved = [];
+			args = args || [];
+			resolved = args;
 			deps = ioc.getFnArgs(reg.value);
-			for (t = 0; t < deps.length; t++) {
-				resolved.push(request.get(deps[t], request));
+			len = Math.max(deps.length, args.length);
+			for (t = 0; t < len; t++) {
+				args[t] !== undefined ?
+					resolved.push(args[t]) :
+					resolved.push(request.get(deps[t]));
 			}
 
 			injectedInstance = function () { };
@@ -58,7 +62,7 @@ var IOC;
 			return reg.instance;
 		}
 	};
-	
+
 
 	ioc.Request = function (container, getFn) {
 		this.visited = {};
@@ -68,16 +72,16 @@ var IOC;
 	};
 
 	ioc.Request.prototype = {
-		get: function (name) {
+		get: function (name, args) {
 			this.visitedArray.push(name);
 			if (this.visited[name]) {
 				throw new Error("Circular reference: " + this.visitedArray.join(" -> "));
 			}
 			this.visited[name] = true;
-			return this.getFn(name, this);
+			return this.getFn(name, this, args);
 		}
 	};
-	
+
 
 	IOC = {
 		register: function (name, value) {
@@ -93,28 +97,29 @@ var IOC;
 				};
 			}
 		},
-		
-		get: function (name) {
+
+		get: function (name, args) {
 			var request = new ioc.Request(ioc.registry, ioc.get);
-			return request.get(name);
+			return request.get(name, args);
 		},
-		
+
 		call: function (method, args, context) {
-			var resolved, deps, t, request;
-			
+			var resolved, deps, t, request, len;
+
 			args = args || [];
 			request = new ioc.Request(ioc.registry, ioc.get);
 			resolved = [];
 			deps = ioc.getFnArgs(method);
-			for (t = 0; t < Math.max(deps.length, args.length); t++) {
-				args[t] !== undefined ? 
+			len = Math.max(deps.length, args.length);
+			for (t = 0; t < len; t++) {
+				args[t] !== undefined ?
 					resolved.push(args[t]) :
 					resolved.push(request.get(deps[t], request));
 			}
-			
+
 			return method.apply(context, resolved);
 		},
-		
+
 		// for testing
 		clear: function () {
 			ioc.registry = {};
