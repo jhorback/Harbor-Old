3e66735113d0725108e01f47e4ca947b83f7f0f0
John Horback <jhorback@gmail.com>
2013-08-27 10:58:24 -0400

    Enabling generic mvcor objects with unique names.

--- Harbor.UI/Scripts/bbext/binders/collectionRendererShim.js
+++ Harbor.UI/Scripts/bbext/binders/collectionRendererShim.js
@@ -3,17 +3,19 @@
 A shim relied on by the collectionRenderer to support
 not having to create a view the root of the collection
 */
-function collectionRendererShim() {
-	
+function collectionRendererShim(viewFactory) {
+	this.viewFactory = viewFactory;
 }
 
 collectionRendererShim.prototype = {
 	parse: function (el) {
-		var collections = el.find("[data-collection]:not([data-templatefor])");
+		var collections = el.find("[data-collection]:not([data-templatefor])"),
+		    viewFactory = this.viewFactory;
 		collections.each(function (i, templateEl) {
-			$(templateEl).attr("data-templatefor", "view");
+			var genericViewName = viewFactory.nextGenericName();
+			$(templateEl).attr("data-templatefor", genericViewName);
 		});
 	}
 };
 
-bbext.shim("collectionRendererShim", [collectionRendererShim]);
+bbext.shim("collectionRendererShim", ["viewFactory", collectionRendererShim]);
