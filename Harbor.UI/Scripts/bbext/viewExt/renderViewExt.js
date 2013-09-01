d6ae72dbd1a2af25835755b271226f00c1e5f44f
John Horback <jhorback@gmail.com>
2013-09-01 18:55:40 -0400

    Added server rendering to the rednerViewExt

--- Harbor.UI/Scripts/bbext/viewExt/renderViewExt.js
+++ Harbor.UI/Scripts/bbext/viewExt/renderViewExt.js
@@ -21,6 +21,16 @@
  *     If the model is not defined but a collection is, the collectionRenderer will be used
  *     to do the rendering.
  *
+ * Server rendering
+ *     fromServer property:
+ *         Adding a property to the view: fromServer: true
+ *         will skip the template rendering.
+ *     renderFromServer method:
+ *         Uses the serverUrl to fetch the new html. Ensures the
+ *         fromServer flag is set to true. Then calls the render method.
+ *     serverUrl method:
+ *          A serverUrl method can be defined to return a url
+ *          used to fetch the new dom contents.
  */
 function renderViewExt(_, $, templateCache, collectionRenderer) {
 	"use strict";
@@ -30,27 +40,30 @@ function renderViewExt(_, $, templateCache, collectionRenderer) {
 			var templateFn,
 			    el,
 			    shims;
-
-			templateFn = templateCache.getTemplateFor(this.name);
-			setModel(this, templateFn.data);
-
+			
 			if (this.collection) {
 				
 				collectionRenderer.render(this);
 
 			} else {
+
+				if (this.fromServer !== true)
+				{
+					templateFn = templateCache.getTemplateFor(this.name);
+					setModel(this, templateFn.data);
+					el = $(templateFn(modelJSON(this.model)));
+					
+
+					// set the element since the template contains the root
+					this.$el.replaceWith(el);
+					this.setElement(el);
+				}
 				
-				el = $(templateFn(modelJSON(this.model)));
-				shims = this.context.get("shims");
-
-				// set the element since the template contains the root
-				this.$el.replaceWith(el);
-				this.setElement(el);
-
 				// add the view to the $el.data (for the shims)
 				this.$el.data("view", this);
 
 				// allow the shims to render
+				shims = this.context.get("shims");
 				shims.render(this.$el, this.model);
 			}
 
@@ -58,6 +71,20 @@ function renderViewExt(_, $, templateCache, collectionRenderer) {
 			this.onRender && this.onRender(this.model || this.collection);
 
 			return this;
+		},
+		
+		renderFromServer: function () {
+			var serverUrl,
+				view = this;
+			
+			this.fromServer = true;
+			serverUrl = this.serverUrl();
+			$.ajax({
+				url: serverUrl,
+				dataType: "html"
+			}).then(function (response) {
+				view.$el.empty().html(response);
+			});
 		}
 	};
 
