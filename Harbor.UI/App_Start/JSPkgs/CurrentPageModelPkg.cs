4822d100ec91c2b5d5b4c5977748d0f6e86d5594
John Horback <jhorback@gmail.com>
2013-09-03 20:04:11 -0400

    Created the currentPageModel module

---
+++ Harbor.UI/App_Start/JSPkgs/CurrentPageModelPkg.cs
@@ -0,0 +1,17 @@
+﻿using Harbor.UI.Models.JSPM;
+
+namespace Harbor.UI.JSPkgs
+{
+	public class CurrentPageModelPkg : JavaScriptPackage
+	{
+		public const string PackageName = "currentPageModel";
+
+		public CurrentPageModelPkg()
+		{
+			Name = PackageName;
+			ScriptBundle = new ModuleScriptBundle(PackageName);
+			Category = Categories.Domain;
+			AddDependency(PageModelPkg.PackageName);
+		}
+	}
+}
