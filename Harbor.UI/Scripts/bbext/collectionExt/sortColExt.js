00b690f8bd9b27facb91bb3cd430633ce2b58de8
John Horback <jhorback@gmail.com>
2013-09-13 23:30:49 -0400

    Adding the initial filter and sort collection extensions.

---
+++ Harbor.UI/Scripts/bbext/collectionExt/sortColExt.js
@@ -0,0 +1,18 @@
+﻿
+
+function sortColExt(mixin) {
+	
+	var sortColExt = {
+		setSort: function (fn) {
+			this.comparator = fn;
+			this.sort();
+			this.trigger("reset");
+		}
+	};
+
+	mixin("collection").register("bbext.sortColExt", sortColExt);
+
+};
+
+
+bbext.config(["mixin", sortColExt]);
