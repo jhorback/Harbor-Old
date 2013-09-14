08a3fc0b80040e79da4e622281c6282dd581db54
John Horback <jhorback@gmail.com>
2013-09-13 23:30:49 -0400

    Adding the initial filter and sort collection extensions.

---
+++ Harbor.UI/Scripts/bbext/collectionExt/filterColExt.js
@@ -0,0 +1,32 @@
+﻿
+
+
+function filterColExt(mixin) {
+
+	var filterColExt = {
+		afterInit: function () {
+			
+			// this.source = new collection(this.models);
+			// add reverse linking to the source - add/remove - 
+
+			// this.source = this.collectionFactory.create(this.name, this.models);
+		},
+		
+		setFilter: function () {
+			//this.currentFilter = filter;
+			//filtered = this.source.filter(filter);
+			//this.set(filtered); // will fire events
+
+		},
+		
+		clearFilter: function () {
+			// this.setFilter(null);
+		}
+	};
+
+	mixin("collection").register("bbext.filterColExt", filterColExt);
+
+};
+
+
+bbext.config(["mixin", filterColExt]);
