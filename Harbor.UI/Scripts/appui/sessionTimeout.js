16ff0037fef927c9cb0eda88535349d9ab0b1923
John Horback <jhorback@gmail.com>
2013-09-18 15:21:19 -0400

    Adjustment to the sessionTimeout script

--- Harbor.UI/Scripts/appui/sessionTimeout.js
+++ Harbor.UI/Scripts/appui/sessionTimeout.js
@@ -5,21 +5,21 @@
  *      start(logoutUrl, timeout);
  *      extend();
  */
- context.module("appui").service("sessionTimeout", ["keepAlive", function (keepAlive) {
+context.module("appui").service("sessionTimeout", ["keepAlive", function (keepAlive) {
 
 	var timeoutId = null;
-	
+
 	return {
 		start: function (logoutUrl, keepAliveUrl, timeout) {
-			
+
 			this.logoutUrl = logoutUrl || this.logoutUrl;
 			this.timeout = Math.max(timeout || this.timeout || (20 * 60 * 1000), 60000);
 
 			keepAlive.stop();
-			keepAlive.start(keepAliveUrl, this.timeout / 2);
+			keepAlive.start(keepAliveUrl, (this.timeout / 2) - 10000); // stagger the keep alive request
 			queueRequest(this.logoutUrl, this.timeout);
 		},
-		
+
 		extend: function () {
 			clearTimeout(timeoutId);
 			this.start();
@@ -28,7 +28,11 @@
 
 	function queueRequest(logoutUrl, timeout) {
 		timeoutId = setTimeout(function () {
-			$.get(logoutUrl).always(function () {
+			$.ajax({
+				url: logoutUrl,
+				data: { sessionTimeout: true },
+				dataType: "html"
+			}).always(function () {
 				var href = window.location.href.split("#")[0];
 				window.location = $.trim(href);
 			});
