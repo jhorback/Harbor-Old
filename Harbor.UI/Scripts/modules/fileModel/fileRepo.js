1524402dfb34ea4cac201d71f520c37ed08bc64d
John Horback <jhorback@gmail.com>
2013-09-19 07:55:13 -0400

    Working on file admin

--- Harbor.UI/Scripts/modules/fileModel/fileRepo.js
+++ Harbor.UI/Scripts/modules/fileModel/fileRepo.js
@@ -7,9 +7,32 @@ fileModel.fileRepo = function (collectionFactory, ajaxRequest) {
 			var files = collectionFactory.create("files");
 			ajaxRequest.handle(files.fetch({ data: data }));
 			return files;
+		},
+
+		getAlbums: function (data) {
+			//sortByAlbum
+			this.getFiles(data).then(function (files) {
+				// keep the source models in sync
+			this.on("all", function (event, model) {
+				if (!this._sync && (event === "add" || event === "remove")) {
+					this.source[event](model);
+				}
+			}, this);
+			});
 		}
 	};
 };
+
+
+
+fileModel.albums = function () {
+
+};
+
+fileModel.albums.prototype = {
+
+};
+
 
 
 
