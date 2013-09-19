13083376d5fb5f015ef7d08b70e5a07871fc745d
John Horback <jhorback@gmail.com>
2013-09-19 16:27:38 -0400

    More work on file admin

---
+++ Harbor.UI/Scripts/appui/slugify.js
@@ -0,0 +1,11 @@
+﻿
+// turns a string into a url/id friendly slug.
+
+function slugify (text) {
+	text=text.replace(/[^-a-zA-Z0-9,&\s]+/ig,'');
+	text=text.replace(/-/gi,"_");
+	text=text.replace(/\s/gi,"-");
+	return text;
+}
+
+appui.register("slugify", slugify, "function");
---
+++ Harbor.UI/Scripts/modules/fileModel/albums.js
@@ -0,0 +1,17 @@
+﻿
+
+fileModel.albums = {
+	model: "album",
+
+	comparator: function (album) {
+		// the album name is a date
+		return new Date(album.get("name")).getTime() * -1;
+	},
+
+	groupBy: function (file) {
+		return file.get("album");
+	}
+};
+
+
+fileModel.collection("albums", fileModel.albums);
