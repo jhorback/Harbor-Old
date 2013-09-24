e3a818bb357dea6407f487a53c9f6e5bf18f0fd5
John Horback <jhorback@gmail.com>
2013-09-24 10:52:01 -0400

    First pass at the text component refactoring

---
+++ Harbor.UI/Scripts/modules/pageEditor/text/text.js
@@ -0,0 +1,27 @@
+﻿
+
+pageEditor.text = function (viewRenderer) {
+	this.viewRenderer = viewRenderer;
+};
+
+pageEditor.text.prototype = {
+	model: "textModel",
+	
+	create: function () {
+		this.open();
+	},
+
+	open: function () {
+		this.view = viewRenderer.create("textView", {
+			el: this.$el,
+			model: this.model
+		});
+	},
+
+	close: function () {
+		this.view.close();
+	}
+};
+
+
+pageEditor.pageComponent("text", ["viewRenderer", pageEditor.text]);
---
+++ Harbor.UI/Scripts/modules/pageEditor/text/textView.js
@@ -0,0 +1,56 @@
+﻿
+
+pageEditor.textView = function (options, currentPageRepo) {
+
+	this.currentPageRepo = currentPageRepo;
+};
+
+pageEditor.textView.prototype = {	
+	onRender: function () {
+		var buttons, richTextEl;
+
+		buttons = [
+			"formatting", "|",
+			"bold", "italic", "deleted", "underline", "|",
+			"unorderedlist", "orderedlist", "outdent", "indent", "|",
+			"horizontalrule", "|",
+			"fontcolor", "backcolor", "|",
+			"alignleft", "aligncenter", "alignright", "justify", "|",
+			"html"
+		];
+       
+		richTextEl = this.$el.find(".richtext");
+		richTextEl.redactor({
+			focus: true,
+			// air: true,
+			// airButtons: buttons,
+			buttons: buttons
+		});
+	},
+
+	onClose: function () {
+		var ctr = this.$el.find(".richtext"),
+		    redactor = ctr.data('redactor'),
+		    html;
+
+		if (redactor) {
+			html = ctr.getCode();
+			if ($.trim($(html).text()) === "") {
+				html = "";
+			}
+
+			this.model.set("text", html);
+			this.model.page.updatePagePreviewText(this.model.get("id"), html);
+			this.currentPageRepo.saveCurrentPage();
+		}
+
+		ctr.destroyEditor && ctr.destroyEditor();
+	}
+};
+
+
+
+pageEditor.view("textView", [
+	"options",
+	pageEditor.textView
+]);
