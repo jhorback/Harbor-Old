38199d7fdb3ad9e814c09d41d5691d6a35af1921
John Horback <jhorback@gmail.com>
2013-09-27 08:19:44 -0400

    Added view

---
+++ Harbor.UI/Scripts/modules/pageEditor/changeLayoutView.js
@@ -0,0 +1,55 @@
+﻿
+pageEditor.component("changeLayout");
+
+
+// options: uicid - the uicid of the content component to update
+pageEditor.changeLayoutView = function (
+	options,
+	currentPageRepo,
+	modelFactory,
+	dialogFactory
+) {
+	var currentPage;
+	
+	this.currentPageRepo = currentPageRepo;
+	this.dialogFactory = dialogFactory;
+	this.model = modelFactory.create("changeLayoutViewModel");
+	
+	currentPage = this.currentPageRepo.getCurrentPage();
+	this.componentModel = currentPage.template.content.get(options.uicid);
+	this.model.setClassNames(this.componentModel.get("classNames"));
+};
+
+
+pageEditor.changeLayoutView.prototype = {
+	
+	submitForm: function (event) {
+		event.preventDefault();
+		this.save();
+	},
+
+	onRender: function () {
+		this.dialog = this.dialogFactory.create(this.$el);
+	},
+
+	save: function () {
+		var classNames = this.model.getClassNames();
+		this.componentModel.set("classNames", classNames);
+		this.currentPageRepo.saveCurrentPage();
+		$("#" + this.componentModel.get("id")).removeClass().addClass(classNames.join(" ") + " uic");
+		this.close();
+	},
+
+	onClose: function () {
+		this.dialog && this.dialog.close();
+	}
+};
+
+
+pageEditor.view("changeLayoutView", [
+	"options",
+	"currentPageRepo",
+	"modelFactory",
+	"dialogFactory",
+	pageEditor.changeLayoutView
+]);
