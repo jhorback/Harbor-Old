2ad8d400b6974f25b51b5ce41a5f71f66574b25c
John Horback <jhorback@gmail.com>
2013-10-05 22:17:36 -0400

    Implemented remove link

---
+++ Harbor.UI/Scripts/modules/pageEditor/links/linksSectionLinksView.js
@@ -0,0 +1,9 @@
+﻿
+pageEditor.linksSectionLinksView = {
+	removeLink: function (event) {
+		event.preventDefault;
+		this.model.collection.remove(this.model);
+	},
+};
+
+pageEditor.view("linksSectionLinksView", pageEditor.linksSectionLinksView);
