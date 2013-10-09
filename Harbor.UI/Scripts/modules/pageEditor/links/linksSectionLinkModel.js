7df8463072112274321cfa03f6096dcdc87d1ca3
John Horback <jhorback@gmail.com>
2013-10-08 23:25:25 -0400

    Working on link sorting

--- Harbor.UI/Scripts/modules/pageEditor/links/linksSectionLinkModel.js
+++ Harbor.UI/Scripts/modules/pageEditor/links/linksSectionLinkModel.js
@@ -9,13 +9,21 @@ pageEditor.linkSectionLinkModel.prototype = {
 	defaults: {
 		pageID: null,
 		text: null,
-		itemClassName: null
+		//
+		itemClassName: null,
+		cid: null
 	},
 	"[itemClassName]": {
 		get: function (value) {
 			var url = "/" + this.get("pageID") + "/" + this.get("text");
 			return decodeURI(this.location.pathname.toLowerCase()).indexOf(url.toLowerCase()) > -1 ?
 				"selected" : null;
+		}
+	},
+	
+	"[cid]": {
+		get: function (value) {
+			return this.cid;
 		}
 	}
 };
