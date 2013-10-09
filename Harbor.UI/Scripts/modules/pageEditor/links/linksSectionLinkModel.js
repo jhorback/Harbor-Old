e997b55dda96654b94f3bdb6687b9c30203dd833
John Horback <jhorback@gmail.com>
2013-10-08 20:40:54 -0400

    Pulled the link model out

---
+++ Harbor.UI/Scripts/modules/pageEditor/links/linksSectionLinkModel.js
@@ -0,0 +1,28 @@
+﻿
+
+pageEditor.linkSectionLinkModel = function (attrs, options, location) {
+	this.location = location;
+};
+
+
+pageEditor.linkSectionLinkModel.prototype = {
+	defaults: {
+		pageID: null,
+		text: null,
+		itemClassName: null
+	},
+	"[itemClassName]": {
+		get: function (value) {
+			var url = "/" + this.get("pageID") + "/" + this.get("text");
+			return decodeURI(this.location.pathname.toLowerCase()).indexOf(url.toLowerCase()) > -1 ?
+				"selected" : null;
+		}
+	}
+};
+
+pageEditor.model("linkSectionLinkModel", [
+	"attrs",
+	"options",
+	"location",
+	pageEditor.linkSectionLinkModel
+]);
--- Harbor.UI/Scripts/modules/pageEditor/links/linksSectionModel.js
+++ Harbor.UI/Scripts/modules/pageEditor/links/linksSectionModel.js
@@ -33,34 +33,6 @@ pageEditor.linksSectionModel.prototype = {
 };
 
 
-pageEditor.linkSectionLinkModel = function (attrs, options, location) {
-	this.location = location;
-};
-
-
-pageEditor.linkSectionLinkModel.prototype = {
-	defaults: {
-		pageID: null,
-		text: null,
-		itemClassName: null
-	},
-	"[itemClassName]": {
-		get: function (value) {
-			var url = "/" + this.get("pageID") + "/" + this.get("text");
-			return decodeURI(this.location.pathname.toLowerCase()).indexOf(url.toLowerCase()) > -1 ?
-				"selected" : null;
-		}
-	}
-};
-
-pageEditor.model("linkSectionLinkModel", [
-	"attrs",
-	"options",
-	"location",
-	pageEditor.linkSectionLinkModel
-]);
-
-
 pageEditor.model("linksSectionModel", [
 	"attrs",
 	"options",
