70e08cefd26af3feeccba68612309aa170732883
John Horback <jhorback@gmail.com>
2013-10-21 21:06:57 -0400

    Added a permission deinied message on login when redirected and authenticated

--- Harbor.UI/Scripts/apps/Session/SignInView.js
+++ Harbor.UI/Scripts/apps/Session/SignInView.js
@@ -14,23 +14,37 @@
 		}
 	},
 	
+	initialize: function () {
+		if (Session.currentUser.get("isAuthenticated") &&
+			window.location.toString().toLowerCase().indexOf("returnurl") > -1) {
+			this.showError("Permission Denied", "You do not have permission to view the requested page.");
+		}
+	},
+	
 	signIn: function () {
-		var displayErrors = _.bind(this.displayErrors, this),
-				clearErrors = _.bind(this.clearErrors, this);
-
+		var showError = _.bind(this.showError, this),
+			clearErrors = _.bind(this.clearErrors, this);
+		
 		Session.signIn(this.model.toJSON(), {
 			clientError: function (error) {
-				context.app("session").call(["modelErrors", function (modelErrors) {
-					var errors = modelErrors.create();
-					errors.add("<h1>Sign in failed</h1>" + error);
-					displayErrors(errors.toJSON());
-				}]);
-				
+				showError("Sign in failed", error);
 			},
 			success: function () {
 				clearErrors();
 			}
 		});
+	},
+	
+	showError: function (error, message) {
+		var displayErrors = _.bind(this.displayErrors, this);
+
+		setTimeout(function () {
+			context.app("session").call(["modelErrors", function (modelErrors) {
+				var errors = modelErrors.create();
+				errors.add("<h1>" + error + "</h1><p>" + message + "</p>");
+				displayErrors(errors.toJSON());
+			}]);
+		}, 1);
 	},
 
 	render: function () {
