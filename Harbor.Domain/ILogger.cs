f731b43c19e06cf3d6f6e242e0ed82ce246a4131
John Horback <jhorback@gmail.com>
2013-10-22 23:21:14 -0400

    Switched to use autofac over structure map

--- Harbor.Domain/ILogger.cs
+++ Harbor.Domain/ILogger.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 
 namespace Harbor.Domain
@@ -13,39 +14,89 @@ namespace Harbor.Domain
 
 		void Error(string message);
 		void Error(string format, params object[] args);
+
+		void Debug(string message);
+		void Debug(string format, params object[] args);
 	}
 
-
+	// jch! - move class
 	public class Logger : ILogger
 	{
+		readonly string typeName = "";
+
+		public Logger() { }
+
+		public Logger(Type type)
+		{
+			typeName = type.FullName;
+		}
+
 		public void Info(string message)
 		{
-			Trace.TraceInformation(message);
+			Trace.TraceInformation(scrub(message));
 		}
 
 		public void Info(string format, params object[] args)
 		{
-			Trace.TraceInformation(format, args);
+			Trace.TraceInformation(scrub(format), args);
 		}
 
 		public void Warn(string message)
 		{
-			Trace.TraceWarning(message);
+			Trace.TraceWarning(scrub(message));
 		}
 
 		public void Warn(string format, params object[] args)
 		{
-			Trace.TraceWarning(format, args);
+			Trace.TraceWarning(scrub(format), args);
 		}
 
 		public void Error(string message)
 		{
-			Trace.TraceError(message);
+			Trace.TraceError(scrub(message));
 		}
 
 		public void Error(string format, params object[] args)
 		{
-			Trace.TraceError(format, args);
+			Trace.TraceError(scrub(format), args);
+		}
+		
+		public void Debug(string message)
+		{
+			Trace.WriteLine(scrub(message));
+			Console.WriteLine(scrub(message));
+		}
+
+		public void Debug(string format, params object[] args)
+		{
+			Trace.WriteLine(format); // jch! just testing - there is a category here too
+			Console.WriteLine(scrub(format), args);
+		}
+
+		private string scrub(string msg)
+		{
+			if (string.IsNullOrEmpty(typeName))
+				return msg;
+			return typeName + " - " + msg;
+		}
+	}
+
+
+	public class HarborTextWriterTraceListener : TextWriterTraceListener
+	{
+		public HarborTextWriterTraceListener(string fileName)
+			: base(fileName) { }
+
+		public override void Write(string message)
+		{
+			base.Write(String.Format("[{0}]:{1}", DateTime.Now, message));
+			Flush();
+		}
+
+		public override void WriteLine(string message)
+		{
+			base.WriteLine(String.Format("[{0}]:{1}", DateTime.Now, message));
+			Flush();
 		}
 	}
 }
---
+++ Harbor.UI/App_Start/AutofacModules/LogInjectionModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using Autofac.Core;
+using Harbor.Domain;
+
+namespace Harbor.UI.AutofacModules
+{
+	public class LogInjectionModule : Module
+	{
+		protected override void AttachToComponentRegistration(IComponentRegistry registry, IComponentRegistration registration)
+		{
+			registration.Preparing += OnComponentPreparing;
+		}
+
+		static void OnComponentPreparing(object sender, PreparingEventArgs e)
+		{
+			var t = e.Component.Activator.LimitType;
+			e.Parameters = e.Parameters.Union(new[]
+			{
+				new ResolvedParameter((p, i) => p.ParameterType == typeof(ILogger), (p, i) => new Logger(t))
+			});
+		}
+	}
+}
