49d3317bd2e08b4259bbe9e48b7bafde92d4323b
John Horback <jhorback@gmail.com>
2013-10-23 08:50:28 -0400

    Working on logging, testing with azure

--- Harbor.Domain/ILogger.cs
+++ Harbor.Domain/ILogger.cs
@@ -6,17 +6,26 @@ namespace Harbor.Domain
 {
 	public interface ILogger
 	{
+		void Debug(string message);
+		void Debug(string format, params object[] args);
+
 		void Info(string message);
 		void Info(string format, params object[] args);
 
 		void Warn(string message);
 		void Warn(string format, params object[] args);
 
+		void Error(Exception exception);
 		void Error(string message);
+		void Error(string message, Exception exception);
 		void Error(string format, params object[] args);
+		void Error(string format, Exception exception, params object[] args);
 
-		void Debug(string message);
-		void Debug(string format, params object[] args);
+		void Fatal(Exception exception);
+		void Fatal(string message);
+		void Fatal(string message, Exception exception);
+		void Fatal(string format, params object[] args);
+		void Fatal(string format, Exception exception, params object[] args);
 	}
 
 	// jch! - move class
@@ -24,26 +33,40 @@ namespace Harbor.Domain
 	{
 		readonly string typeName = "";
 
-		public Logger() { }
-
 		public Logger(Type type)
 		{
 			typeName = type.FullName;
 		}
 
+
+		public void Debug(string message)
+		{
+			Debug(message, null);
+		}
+
+		public void Debug(string format, params object[] args)
+		{
+			Trace.WriteLine(string.Format(scrub(format), args), "Debug");
+			Console.WriteLine(scrub(format), args);
+		}
+
+
 		public void Info(string message)
 		{
-			Trace.TraceInformation(scrub(message));
+			Info(message, null);
 		}
 
 		public void Info(string format, params object[] args)
 		{
+			Trace.WriteLine(string.Format(scrub(format), args), "Info");
+			Trace.WriteLine(string.Format(scrub(format), args), "Information");
 			Trace.TraceInformation(scrub(format), args);
 		}
 
+
 		public void Warn(string message)
 		{
-			Trace.TraceWarning(scrub(message));
+			Warn(message, null);
 		}
 
 		public void Warn(string format, params object[] args)
@@ -51,27 +74,73 @@ namespace Harbor.Domain
 			Trace.TraceWarning(scrub(format), args);
 		}
 
+
+		public void Error(Exception exception)
+		{
+			Error(null, exception, null);
+		}
+
 		public void Error(string message)
 		{
-			Trace.TraceError(scrub(message));
+			Error(message, null, null);
+		}
+
+		public void Error(string message, Exception exception)
+		{
+			Error(message, exception, null);
 		}
 
 		public void Error(string format, params object[] args)
 		{
-			Trace.TraceError(scrub(format), args);
-		}
-		
-		public void Debug(string message)
-		{
-			Trace.WriteLine(scrub(message));
-			Console.WriteLine(scrub(message));
+			Error(format, null, args);
 		}
 
-		public void Debug(string format, params object[] args)
+		public void Error(string format, Exception exception, params object[] args)
 		{
-			Trace.WriteLine(format); // jch! just testing - there is a category here too
-			Console.WriteLine(scrub(format), args);
+			var message = string.IsNullOrEmpty(format) ? "" : string.Format(scrub(format), args);
+			if (exception != null)
+			{
+				message = String.Format("{0}{1}{2}", message, exception.Message, exception.StackTrace);				
+			}
+			Trace.TraceError(message);
 		}
+
+
+		public void Fatal(Exception exception)
+		{
+			Fatal(null, exception, null);
+		}
+
+		public void Fatal(string message)
+		{
+			Fatal(message, null, null);
+		}
+
+		public void Fatal(string message, Exception exception)
+		{
+			Fatal(message, exception, null);
+		}
+
+		public void Fatal(string format, params object[] args)
+		{
+			Fatal(format, null, args);
+		}
+
+		public void Fatal(string format, Exception exception, params object[] args)
+		{
+			var message = string.IsNullOrEmpty(format) ? "" : string.Format(scrub(format), args);
+			if (exception != null)
+			{
+				message = String.Format("FATAL ERROR - {0} {1} {2}", message, exception.Message, exception.StackTrace);
+			}
+			else
+			{
+				message = String.Format("FATAL ERROR - {0}", message);
+			}
+			Trace.TraceError(message);
+		}
+		
+		
 
 		private string scrub(string msg)
 		{
@@ -82,6 +151,9 @@ namespace Harbor.Domain
 	}
 
 
+
+
+
 	public class HarborTextWriterTraceListener : TextWriterTraceListener
 	{
 		public HarborTextWriterTraceListener(string fileName)
@@ -90,13 +162,11 @@ namespace Harbor.Domain
 		public override void Write(string message)
 		{
 			base.Write(String.Format("[{0}]:{1}", DateTime.Now, message));
-			Flush();
 		}
 
 		public override void WriteLine(string message)
 		{
 			base.WriteLine(String.Format("[{0}]:{1}", DateTime.Now, message));
-			Flush();
 		}
 	}
 }
