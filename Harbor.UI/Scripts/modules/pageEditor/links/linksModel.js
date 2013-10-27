3963db9bc0c43534e70cead28ec46322cee3acaf
John Horback <jhorback@gmail.com>
2013-10-27 10:30:11 -0400

    Updated sorting for new link sections to appear at the bottom

--- Harbor.UI/Scripts/modules/pageEditor/links/linksModel.js
+++ Harbor.UI/Scripts/modules/pageEditor/links/linksModel.js
@@ -7,6 +7,9 @@ pageEditor.linksModel = function (attrs, options, collectionFactory, currentPage
 		comparator: function (model) {
 			var node = $("[data-cid=" + model.cid + "]");
 			var index = node.index();
+			if (index === -1) {
+				index = 1000;
+			}
 			return index;
 		}
 	});
