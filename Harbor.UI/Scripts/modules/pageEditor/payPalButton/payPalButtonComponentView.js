b1fb4cac558abb27ad2d9cfd28b6c45b048b5310
John Horback <jhorback@gmail.com>
2013-11-08 00:02:27 -0500

    added the initial paypal button preview while editing

--- Harbor.UI/Scripts/modules/pageEditor/payPalButton/payPalButtonComponentView.js
+++ Harbor.UI/Scripts/modules/pageEditor/payPalButton/payPalButtonComponentView.js
@@ -6,11 +6,12 @@ pageEditor.component("payPalButtonComponent", {
 
 
 
-pageEditor.payPalButtonComponentView = function (options, currentPageRepo, payPalButtonRepo) {
+pageEditor.payPalButtonComponentView = function (options, currentPageRepo, payPalButtonRepo, viewRenderer) {
 
 	this.componentModel = options.componentModel;
 	this.currentPageRepo = currentPageRepo;
 	this.payPalButtonRepo = payPalButtonRepo;
+	this.viewRenderer = viewRenderer;
 };
 
 
@@ -25,6 +26,13 @@ pageEditor.payPalButtonComponentView.prototype = {
 		this.bindAll("saveButton", "saveComponentModel");
 		this.listenTo(this.model, "change:id", this.saveComponentModel);
 		this.listenTo(this.model, "change", this.saveButton);
+		
+		// jch! cleanup
+		var previewEl = this.$("[data-rel=buttonPreview]");
+		this.viewRenderer.render("payPalButtonView", {
+			el: previewEl,
+			model: this.model
+		});
 	},
 	
 	saveComponentModel: function () {
@@ -48,5 +56,53 @@ pageEditor.view("payPalButtonComponentView", [
 	"options",
 	"currentPageRepo",
 	"payPalButtonRepo",
+	"viewRenderer",
 	pageEditor.payPalButtonComponentView
+]);
+
+
+
+pageEditor.payPalButtonView = function (options, appurl) {
+	this.appurl = appurl;
+};
+
+pageEditor.payPalButtonView.prototype = {
+	initialize: function () {
+		this.listenTo(this.model, "change", this.render);
+	},
+	render: function () {
+		var merchantID = "get it";
+		var str = '<script src="' +
+			this.appurl.get("scripts/paypal-button-minicart.min.js?merchant=" + merchantID) + '" ' +
+			'data-button="' + this.model.get("buttonType") + '"' +
+			'data-name="' + this.model.get("name") + '"' +
+			'data-quantity="1"' +
+			'data-amount="' + this.model.get("price") + '"' +
+			'data-currency="USD"' +
+			'></script>';
+		/*
+		attrs["data-button"] = button.ButtonType;
+			attrs["data-name"] = button.Name;
+			attrs["data-quantity"] = 1;
+			attrs["data-amount"] = button.Price;
+			attrs["data-currency"] = "USD";
+			if (button.ShippingOverride != null)
+			{
+				attrs["data-shipping"] = button.ShippingOverride;
+			}
+			if (button.TaxOverride != null)
+			{
+				attrs["data-tax"] = button.TaxOverride;
+			}*/
+
+		this.$el.empty();
+		this.$el.append($(str));
+	}
+};
+
+
+pageEditor.view("payPalButtonView", [
+	"options",
+	"appurl",
+	pageEditor.payPalButtonView
 ]);
