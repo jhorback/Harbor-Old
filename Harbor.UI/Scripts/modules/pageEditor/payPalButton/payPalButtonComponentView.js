9c1e75851f1d05d90e1fdaa962dce9dc98fbddba
John Horback <jhorback@gmail.com>
2013-11-08 08:50:57 -0500

    Progress on the pay pal button view

--- Harbor.UI/Scripts/modules/pageEditor/payPalButton/payPalButtonComponentView.js
+++ Harbor.UI/Scripts/modules/pageEditor/payPalButton/payPalButtonComponentView.js
@@ -41,8 +41,10 @@ pageEditor.payPalButtonComponentView.prototype = {
 	},
 	
 	saveButton: function () {
-		if (this.model.synced && this.model.isValid()) {
-			this.payPalButtonRepo.saveButton(this.model);
+		if ((this.model.synced || this.model.isNew())) {
+			if (this.isModelValid()) {
+				this.payPalButtonRepo.saveButton(this.model);
+			}
 		}
 	},
 	
@@ -58,51 +60,4 @@ pageEditor.view("payPalButtonComponentView", [
 	"payPalButtonRepo",
 	"viewRenderer",
 	pageEditor.payPalButtonComponentView
-]);
-
-
-
-pageEditor.payPalButtonView = function (options, appurl) {
-	this.appurl = appurl;
-};
-
-pageEditor.payPalButtonView.prototype = {
-	initialize: function () {
-		this.listenTo(this.model, "change", this.render);
-	},
-	render: function () {
-		var merchantID = "get it";
-		var str = '<script src="' +
-			this.appurl.get("scripts/paypal-button-minicart.min.js?merchant=" + merchantID) + '" ' +
-			'data-button="' + this.model.get("buttonType") + '"' +
-			'data-name="' + this.model.get("name") + '"' +
-			'data-quantity="1"' +
-			'data-amount="' + this.model.get("price") + '"' +
-			'data-currency="USD"' +
-			'></script>';
-		/*
-		attrs["data-button"] = button.ButtonType;
-			attrs["data-name"] = button.Name;
-			attrs["data-quantity"] = 1;
-			attrs["data-amount"] = button.Price;
-			attrs["data-currency"] = "USD";
-			if (button.ShippingOverride != null)
-			{
-				attrs["data-shipping"] = button.ShippingOverride;
-			}
-			if (button.TaxOverride != null)
-			{
-				attrs["data-tax"] = button.TaxOverride;
-			}*/
-
-		this.$el.empty();
-		this.$el.append($(str));
-	}
-};
-
-
-pageEditor.view("payPalButtonView", [
-	"options",
-	"appurl",
-	pageEditor.payPalButtonView
 ]);
