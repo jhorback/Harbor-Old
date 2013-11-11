8bdccde91772296491c152124ee0827b38d1f5bd
John Horback <jhorback@gmail.com>
2013-11-11 14:56:28 -0500

    Added missing file

---
+++ Harbor.Domain/Pages/Components/ProductLink.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Harbor.Domain.Pages.Components
+{
+	public class ProductLink : ContentComponent
+	{
+		public const string KEY = "productLink";
+
+		public override string Key
+		{
+			get { return KEY; }
+		}
+
+		public override string Name
+		{
+			get
+			{
+				return "Product Link";
+			}
+		}
+
+		public override string Description
+		{
+			get
+			{
+				return "Add a link to page that contains a PayPal button.";
+			}
+		}
+
+		public override Type PageComponent
+		{
+			get { return typeof(PageComponents.PageLink); }
+		}
+	}
+}
+
