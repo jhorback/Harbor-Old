48029be5327e48f4b042eeea4f1ad142be62029e
John Horback <jhorback@gmail.com>
2013-11-18 14:28:11 -0500

    Created the PagedResult and parseColExt to handle pagination

---
+++ Harbor.UI/Models/Shared/PagedResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Harbor.UI.Models
+{
+	/// <summary>
+	/// A dto that contains a paged results with a count of the
+	/// total number of records.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class PagedResultDto<T>
+	{
+		public int totalCount { get; set; }
+		public IEnumerable<T> results { get; set; }
+	}
+}
---
+++ Harbor.UI/Scripts/bbext/collectionExt/parseColExt.js
@@ -0,0 +1,23 @@
+﻿/*
+  Overrides the parse method.
+  Tests if results are being passed as a property to secure the data.
+  Also looks for totalCount and sets it as a property on the collection for paging.
+*/
+function parseColExt(mixin) {
+	
+	var parseColExt = {
+		parse: function (resp) {
+			if (resp.results) {
+				this.totalCount = resp.totalCount;
+				return resp.results;
+			}
+			return resp;
+		}
+	};
+
+	mixin("collection").register("bbext.parseColExt", parseColExt);
+
+};
+
+
+bbext.config(["mixin", parseColExt]);
