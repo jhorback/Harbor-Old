9454f2f9f5192a76150380bc74be9045f56bdd91
John Horback <jhorback@gmail.com>
2013-11-23 08:22:01 -0500

    Added the post action methods for models and collections

---
+++ Harbor.UI/Scripts/bbext/modelExt/postActionModelExt.js
@@ -0,0 +1,35 @@
+﻿/*
+	Adds a posAction method to a model.
+	
+	Example url: /api/users/5/disable
+		- api/users is the url of the collection
+		- disable is the action passed to postBatchAction
+*/
+function postActionModelExt(mixin) {
+	
+	var postActionModelExt = {
+		postAction: function (action, data) {
+			var root = this.urlRoot || (this.collection && this.collection.url),
+				url;
+		
+			if (!root) {
+				throw new Error("A url is required for postAction.");
+			}
+
+			url = root + "/" + this.get("id") + "/" + action;
+			return this.sync(action, this, {
+				url: url,
+				type: "POST",
+				dataType: "json",
+				contentType: "application/json",
+				data: JSON.stringify(data)
+			});
+		}
+	};
+
+	mixin("model").register("bbext.postActionModelExt", postActionModelExt);
+
+};
+
+
+bbext.config(["mixin", postActionModelExt]);
