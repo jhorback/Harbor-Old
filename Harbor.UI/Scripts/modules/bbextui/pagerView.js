0e65361e3375e30b70b9fd07bd97e1a03a21704c
John Horback <jhorback@gmail.com>
2013-11-23 08:22:39 -0500

    Started the bbextui module with the pager model, view, and select list

---
+++ Harbor.UI/Scripts/modules/bbextui/pagerView.js
@@ -0,0 +1,23 @@
+﻿
+
+
+bbext.pagerView = function () {
+
+};
+
+
+bbext.pagerView.prototype = {	
+	next: function () {
+		this.model.next();
+	},
+	
+	previous: function () {
+		this.model.previous();
+	}
+};
+
+
+bbext.view("pagerView", [
+	"options",
+	bbext.pagerView
+]);
