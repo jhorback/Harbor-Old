ef9335044984a97d6efa894666619df788ade0d5
John Horback <jhorback@gmail.com>
2014-03-13 21:34:28 -0400

    Added the base pipeline

---
+++ Harbor.Domain/Pipeline/BasePipeline.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Harbor.Domain.Pipeline
+{
+	public abstract class BasePipeline<T> : IPipeline<T>
+	{
+		protected BasePipeline(IObjectFactory objectFactory)
+		{
+			handlers = new List<IPipelineHanlder<T>>();
+			this.objectFactory = objectFactory;
+		}
+
+		private readonly IObjectFactory objectFactory;
+		private List<IPipelineHanlder<T>> handlers { get; set; }
+
+		public void AddHandler<TH>() where TH : IPipelineHanlder<T>
+		{
+			handlers.Add(this.objectFactory.GetInstance<TH>());
+		}
+
+		public void Execute(IPipelineContext<T> context)
+		{
+			handlers.ForEach(h => h.Execute(context));
+		}
+
+		public void Execute(T target)
+		{
+			var context = new PipelineContext<T>(target);
+			Execute(context);
+		}
+	}
+}
