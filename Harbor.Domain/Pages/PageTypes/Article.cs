e3d14ba8fa9509172056a17e1d894aa459cc068a
John Horback <jhorback@gmail.com>
2014-03-30 23:22:32 -0400

    Working on the page layout

--- Harbor.Domain/Pages/PageTypes/Article.cs
+++ Harbor.Domain/Pages/PageTypes/Article.cs
@@ -25,5 +25,19 @@ namespace Harbor.Domain.Pages.PageTypes
 				.AddContent(Components.Image.KEY)
 				.AddContent(Components.Text.KEY);
 		}
+
+		public override void OnPageCreate(Pages.Page page)
+		{
+			// jch! need
+			page.Layout.DisplayProperties = LayoutDisplayProperties.ContentCentered | LayoutDisplayProperties.NoAside;
+			page.Layout.h
+
+			Context.SetLayout(LayoutDisplayProperties.ContentCentered | LayoutDisplayProperties.NoAside)
+				.SetHeader(Components.Title.KEY)
+				.AddContent(Components.Image.KEY)
+				.AddContent(Components.Text.KEY);
+
+
+		}
 	}
 }
