f418d6beafac78a189b2babc6856104094595b3e
John Horback <jhorback@gmail.com>
2014-04-05 10:48:37 -0400

    Added the page content repository interface

---
+++ Harbor.Domain/Pages/IPageContentRepository.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Harbor.Domain.Pages
+{
+	/// <summary>
+	/// Responsible for instantiating the different types of PageContent.
+	/// Allows for a PageContent to have custom dependencies.
+	/// </summary>
+	public interface IPageContentRepository
+	{
+		PageContent GetContent(string key, Page page, string uicid);
+		T GetContent<T>(Page page, string uicid) where T: PageContent;
+	}
+}
