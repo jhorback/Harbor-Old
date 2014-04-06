375e3a6f063ca3e49cfdc9fcb1af49d4b1294acf
John Horback <jhorback@gmail.com>
2014-04-06 15:47:21 -0400

    Moved new context classes to their own file

---
+++ Harbor.Domain/Pages/PageTypeLayoutContext.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Harbor.Domain.Pages
+{
+	public class PageTypeLayoutContext
+	{
+		public PageTypeLayoutContext(Page page)
+		{
+			Page = page;
+		}
+
+		public Page Page { get; set; }
+
+		public PageTypeLayoutContext SetLayout(PageLayout.LayoutDisplayProperties layout)
+		{
+			Page.Layout.DisplayProperties = layout;
+			return this;
+		}
+
+		public PageTypeLayoutContext SetHeader(string type)
+		{
+			Page.Layout.HeaderKey = type;
+			return this;
+		}
+
+		public PageTypeLayoutContext SetAside(string type)
+		{
+			Page.Layout.AsideKey = type;
+			return this;
+		}
+	}
+}
--- Harbor.Domain/Pages/PageTypeTemplateContext.cs
+++ Harbor.Domain/Pages/PageTypeTemplateContext.cs
@@ -1,35 +1,6 @@
 ﻿
 namespace Harbor.Domain.Pages
 {
-	//jch! - move / rename these classes
-	public class PageTypeLayoutContext
-	{
-		public PageTypeLayoutContext(Page page)
-		{
-			Page = page;
-		}
-
-		public Page Page { get; set; }
-
-		public PageTypeLayoutContext SetLayout(PageLayout.LayoutDisplayProperties layout)
-		{
-			Page.Layout.DisplayProperties = layout;
-			return this;
-		}
-
-		public PageTypeLayoutContext SetHeader(string type)
-		{
-			Page.Layout.HeaderKey = type;
-			return this;
-		}
-
-		public PageTypeLayoutContext SetAside(string type)
-		{
-			Page.Layout.AsideKey = type;
-			return this;
-		}
-	}
-
 	public class PageTypeTemplateContext
 	{
 		public PageTypeTemplateContext(Page page)
