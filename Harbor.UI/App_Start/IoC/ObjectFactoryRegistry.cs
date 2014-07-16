0ee32ee5d475ecb0def39ad1cf839c7e14825593
John Horback <jhorback@gmail.com>
2014-07-15 22:47:48 -0400

    Progress with ioc scoping issues

--- Harbor.UI/App_Start/IoC/ObjectFactoryRegistry.cs
+++ Harbor.UI/App_Start/IoC/ObjectFactoryRegistry.cs
@@ -1,5 +1,7 @@
-﻿using Harbor.Domain;
+﻿using Harbor.Data;
+using Harbor.Domain;
 using StructureMap.Configuration.DSL;
+using StructureMap.Web;
 
 namespace Harbor.UI.IoC
 {
@@ -8,6 +10,16 @@ namespace Harbor.UI.IoC
 		public ObjectFactoryRegistry()
 		{
 			For<IObjectFactory>().Use(s => new StructureMapObjectFactory());
+		}
+	}
+
+	public class HarborContextRegistry : Registry
+	{
+		public HarborContextRegistry()
+		{
+			For<HarborContext>()
+				.Use(new HarborContext())
+				.SetLifecycleTo(WebLifecycles.HttpContext);
 		}
 	}
 }
