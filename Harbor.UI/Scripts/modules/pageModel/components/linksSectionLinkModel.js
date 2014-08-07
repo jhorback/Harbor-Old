d4c3eaa0225f82370a3ea033f13493a9db10852a
John Horback <jhorback@gmail.com>
2014-08-06 20:34:54 -0400

    Created the links section links collection

--- Harbor.UI/Scripts/modules/pageModel/components/linksSectionLinkModel.js
+++ Harbor.UI/Scripts/modules/pageModel/components/linksSectionLinkModel.js
@@ -51,3 +51,17 @@ pageModel.model("linkSectionLinkModel", [
 	"pageurl",
 	pageModel.linkSectionLinkModel
 ]);
+
+
+pageModel.linkSectionLinkCollection = {
+	model: "linkSectionLinkModel",
+	comparator: function (model) {
+		var node = $("#" + model.cid);
+		var index = node.index();
+		return index;
+	}
+};
+pageModel.collection("linkSectionLinkCollection",
+	pageModel.linkSectionLinkCollection
+);
+
