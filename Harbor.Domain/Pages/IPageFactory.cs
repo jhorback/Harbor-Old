74a682aff203768edca819a836eb580ad1a72e51
John Horback <jhorback@gmail.com>
2014-08-07 10:42:04 -0400

    Updated the page repository to not use the page factory direclty

--- Harbor.Domain/Pages/IPageFactory.cs
+++ Harbor.Domain/Pages/IPageFactory.cs
@@ -4,5 +4,6 @@ namespace Harbor.Domain.Pages
 	public interface IPageFactory : IFactory<Page>
 	{
 		Page Create(string userName, string pageTypeKey, string title, bool publish);
+		Page Create(string userName, string pageTypeKey, string title, bool publish, int pageLayoutId);
 	}
 }
