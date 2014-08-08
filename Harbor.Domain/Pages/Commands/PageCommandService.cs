8896d31809bbdac25f35e7633f8b0c3525ac4a40
John Horback <jhorback@gmail.com>
2014-08-07 20:57:33 -0400

    Added initial command handling for pages

---
+++ Harbor.Domain/Pages/Commands/PageCommandService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Harbor.Domain.Pages
+{
+	public class PageCommandService : IPageCommandService
+	{
+		private readonly Dictionary<string, object> handlers = new Dictionary<string, object>();
+		// handlers may be handlerTypes
+		// then have a handlers cache
+		// look in cache first if not found create from handlerTypes.
+
+
+		public PageCommandService()
+		{
+
+		}
+
+		public void Execute(IPageCommand command)
+		{
+			var commandName = command.GetType().Name.ToLower();
+			var handler = handlers.ContainsKey(commandName) ? handlers[commandName] : null;
+			if (handler != null)
+			{
+				// handler.Execute(command);
+			}
+		}
+	}
+}
