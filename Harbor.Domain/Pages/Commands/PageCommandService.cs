12804b12046fe223655f909c77735445c2e62a01
John Horback <jhorback@gmail.com>
2014-08-08 12:19:22 -0400

    Implemented the page command service execute method

--- Harbor.Domain/Pages/Commands/PageCommandService.cs
+++ Harbor.Domain/Pages/Commands/PageCommandService.cs
@@ -1,30 +1,38 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Harbor.Domain.Pages
 {
 	public class PageCommandService : IPageCommandService
 	{
+		private readonly ReflectionUtils _reflectionUtils;
+		private readonly ILogger _logger;
 		private readonly Dictionary<string, object> handlers = new Dictionary<string, object>();
 		// handlers may be handlerTypes
 		// then have a handlers cache
 		// look in cache first if not found create from handlerTypes.
 
 
-		public PageCommandService()
+		public PageCommandService(ReflectionUtils reflectionUtils, ILogger logger)
 		{
-
+			_reflectionUtils = reflectionUtils;
+			_logger = logger;
 		}
 
 		public void Execute(IPageCommand command)
 		{
-
 			var commandName = command.GetType().Name.ToLower();
 			var handler = handlers.ContainsKey(commandName) ? handlers[commandName] : null;
 			if (handler != null)
 			{
-				// handler.Execute(command);
+				try
+				{
+					_reflectionUtils.InvokeMethod(handler, "Execute", new [] { command });
+				}
+				catch (Exception e)
+				{
+					_logger.Error(e);
+				}
 			}
 		}
 	}
