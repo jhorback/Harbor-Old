7036289f863bd85fb3768febf8ecf27ac6d333f5
John Horback <jhorback@gmail.com>
2014-08-09 01:07:51 -0400

    Added tests for the page command service

---
+++ Harbor.Domain/Pages/Commands/TestCommand.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Harbor.Domain.Pages
+{
+	public class TestCommand : IPageCommand
+	{
+		public string TestMessage { get; set; }
+	}
+
+	public class TestCommandHandler : IPageCommandHandler<TestCommand>
+	{
+		public void Execute(TestCommand command)
+		{
+			command.TestMessage = "Command Executed";
+		}
+	}
+}
