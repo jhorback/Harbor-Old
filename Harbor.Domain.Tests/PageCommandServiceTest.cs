efd88280d0e5803ec79dc74ec245995d0888969f
John Horback <jhorback@gmail.com>
2014-08-09 01:07:51 -0400

    Added tests for the page command service

---
+++ Harbor.Domain.Tests/PageCommandServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Harbor.Domain.Diagnostics;
+using Harbor.Domain.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Harbor.Domain.Tests
+{
+	[TestClass]
+	public class PageCommandServiceTest
+	{
+		[TestMethod]
+		public void Execute_PullsHandlers_CallsExecute()
+		{
+			var reflectionUtils = new ReflectionUtils();
+			var logger = new Logger(typeof(PageCommandServiceTest));
+			var mockObjectFactory = new Mock<IObjectFactory>();
+			
+			mockObjectFactory.Setup(o => o.GetInstance(typeof(TestCommandHandler))).Returns(new TestCommandHandler());
+			
+			var pcs = new PageCommandService(reflectionUtils, logger, mockObjectFactory.Object);
+			var testCommand = new TestCommand { TestMessage = "Initial Message" };
+			
+			pcs.Execute(testCommand);
+
+			Assert.IsTrue(testCommand.TestMessage == "Command Executed");
+		}
+	}
+}
