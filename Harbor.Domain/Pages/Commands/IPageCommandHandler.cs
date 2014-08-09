2704d4fb4599d49cc7b20b1c9bb76de01ed758ee
John Horback <jhorback@gmail.com>
2014-08-09 01:07:29 -0400

    Got the page command service working

--- Harbor.Domain/Pages/Commands/IPageCommandHandler.cs
+++ Harbor.Domain/Pages/Commands/IPageCommandHandler.cs
@@ -1,7 +1,12 @@
 ﻿namespace Harbor.Domain.Pages
 {
-	public interface IPageCommandHandler<in T> where T : IPageCommand
+	public interface IPageCommandHandler<in T> : IPageCommandHandler
 	{
 		void Execute(T command);
+	}
+
+	public interface IPageCommandHandler
+	{
+		
 	}
 }
--- Harbor.Domain/Pages/Commands/PageCommandService.cs
+++ Harbor.Domain/Pages/Commands/PageCommandService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Harbor.Domain.Pages
 {
@@ -7,16 +8,40 @@ namespace Harbor.Domain.Pages
 	{
 		private readonly ReflectionUtils _reflectionUtils;
 		private readonly ILogger _logger;
-		private readonly Dictionary<string, object> handlers = new Dictionary<string, object>();
-		// handlers may be handlerTypes
-		// then have a handlers cache
-		// look in cache first if not found create from handlerTypes.
+		private readonly IObjectFactory _objectFactory;
+		private readonly Dictionary<string, Type> handlers = new Dictionary<string, Type>();
+		
 
-
-		public PageCommandService(ReflectionUtils reflectionUtils, ILogger logger)
+		public PageCommandService(ReflectionUtils reflectionUtils, ILogger logger, IObjectFactory objectFactory)
 		{
 			_reflectionUtils = reflectionUtils;
 			_logger = logger;
+			_objectFactory = objectFactory;
+
+			findHandlers();
+		}
+
+		void findHandlers()
+		{
+			var commandHandlers = _reflectionUtils.FindTypesImplementingInterface(typeof(IPageCommandHandler));
+			foreach (var handler in commandHandlers)
+			{
+				var types = _reflectionUtils.GetTypeParameters(handler, typeof(IPageCommandHandler<>));
+				if (types == null)
+				{
+					_logger.Warn("A page command handler did not implement a generic type. Handler type: {0}", handler.Name);
+					continue;
+				}
+
+				var type = types.FirstOrDefault();
+				if (type == null)
+				{
+					_logger.Warn("A page command handler did not implement a generic type. Handler type: {0}", handler.Name);
+					continue;
+				}
+
+				handlers.Add(type.Name.ToLower(), handler);
+			}
 		}
 
 		public void Execute(IPageCommand command)
@@ -25,9 +50,10 @@ namespace Harbor.Domain.Pages
 			var handler = handlers.ContainsKey(commandName) ? handlers[commandName] : null;
 			if (handler != null)
 			{
+				var handlerInstance = _objectFactory.GetInstance(handler);
 				try
 				{
-					_reflectionUtils.InvokeMethod(handler, "Execute", new [] { command });
+					_reflectionUtils.InvokeMethod(handlerInstance, "Execute", new[] { command });
 				}
 				catch (Exception e)
 				{
