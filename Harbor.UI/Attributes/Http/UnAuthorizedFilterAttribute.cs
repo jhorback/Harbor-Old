98bc3a8f59cbea6bc237f9448f94c7786fa483ec
John Horback <jhorback@gmail.com>
2014-08-13 23:12:12 -0400

    Added the unauthorized filter attribute and filter

---
+++ Harbor.UI/Attributes/Http/UnAuthorizedFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Http.Filters;
+using Harbor.UI.Extensions;
+
+namespace Harbor.UI.Http
+{
+	public class UnAuthorizedFilterAttribute : ExceptionFilterAttribute
+	{
+		public override void OnException(HttpActionExecutedContext context)
+		{
+			if (context.Exception != null && context.Exception.GetType() == typeof(UnauthorizedAccessException))
+			{
+				context.Response = context.Request.CreateUnauthorizedResponse(context.Exception as UnauthorizedAccessException);
+			}
+		}
+	}
+}
