5510950572c209cd3df38ec20bca0a1f0fdb534f
John Horback <jhorback@gmail.com>
2014-08-14 15:39:15 -0400

    Fixed the unit tests from the latest changes

--- Harbor.Domain.Tests/PageCommandServiceTest.cs
+++ Harbor.Domain.Tests/PageCommandServiceTest.cs
@@ -3,43 +3,34 @@ using Harbor.Domain.Pages;
 using Harbor.Domain.Pages.Commands;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Moq.AutoMock;
 
 namespace Harbor.Domain.Tests
 {
 	[TestClass]
 	public class PageCommandServiceTest
 	{
-		//[TestMethod]
-		//public void Execute_PullsHandlers_CallsExecute()
-		//{
-		//	var reflectionUtils = new ReflectionUtils();
-		//	var logger = new Logger(typeof(PageCommandServiceTest));
-		//	var mockObjectFactory = new Mock<IObjectFactory>();
+		[TestMethod]
+		public void Execute_PullsHandlers_CallsExecute()
+		{
+			var reflectionUtils = new ReflectionUtils();
+			var logger = new Logger(typeof(PageCommandServiceTest));
+			var mockObjectFactory = new Mock<IObjectFactory>();
+			var commandRepository = new CommandContainerRepository(mockObjectFactory.Object, new Mock<IMemCache>().Object);
+			// var mockCommandContainerRepository = new Mock<CommandContainerRepository>();
+			var commandContainer = new CommandContainer(reflectionUtils, logger, mockObjectFactory.Object);
+
+
+			mockObjectFactory.Setup(o => o.GetInstance(typeof(TestCommandHandler))).Returns(new TestCommandHandler());
+			mockObjectFactory.Setup(o => o.GetInstance<CommandContainer>()).Returns(commandContainer);
+
+
+			var pcs = new PageCommandService(commandRepository);
+			var testCommand = new TestCommand { TestMessage = "Initial Message" };
 			
-		//	mockObjectFactory.Setup(o => o.GetInstance(typeof(TestCommandHandler))).Returns(new TestCommandHandler());
-			
-		//	var pcs = new PageCommandService(reflectionUtils, logger, mockObjectFactory.Object);
-		//	var testCommand = new TestCommand { TestMessage = "Initial Message" };
-			
-		//	pcs.Execute(testCommand);
+			pcs.Execute(testCommand);
 
-		//	Assert.IsTrue(testCommand.TestMessage == "Command Executed");
-		//}
-
-		//[TestMethod]
-		//public void GetCommandType_ReturnsTheType()
-		//{
-		//	var reflectionUtils = new ReflectionUtils();
-		//	var logger = new Logger(typeof(PageCommandServiceTest));
-		//	var mockObjectFactory = new Mock<IObjectFactory>();
-
-		//	mockObjectFactory.Setup(o => o.GetInstance(typeof(TestCommandHandler))).Returns(new TestCommandHandler());
-
-		//	var pcs = new PageCommandService(reflectionUtils, logger, mockObjectFactory.Object);
-
-		//	var type = pcs.GetCommandType("TestCommand");
-
-		//	Assert.AreEqual(typeof(TestCommand), type);
-		//}
+			Assert.AreEqual(testCommand.TestMessage, "Command Executed");
+		}
 	}
 }
