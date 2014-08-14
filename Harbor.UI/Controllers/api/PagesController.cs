8d331232ca267765fbc6091dbdb649ef382acff2
John Horback <jhorback@gmail.com>
2014-08-14 10:06:33 -0400

    Moved the page commands into a separate controller

--- Harbor.UI/Controllers/api/PagesController.cs
+++ Harbor.UI/Controllers/api/PagesController.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using System.Web.Http.ModelBinding;
-using System.Windows.Input;
 using AutoMapper;
 using Harbor.Domain;
 using Harbor.Domain.Pages;
-using Harbor.Domain.Pages.Commands;
 using Harbor.Domain.Security;
 using Harbor.UI.Extensions;
 using Harbor.UI.Models;
-using Newtonsoft.Json.Linq;
 
 namespace Harbor.UI.Controllers.Api
 {
@@ -109,59 +103,5 @@ namespace Harbor.UI.Controllers.Api
 			_pageRep.Save();
 			return Request.CreateResponse(HttpStatusCode.NoContent);
         }
-
-
-
-		#region page commands
-		[HttpPost, Http.PagePermit(Permissions.All)]
-		public HttpResponseMessage AddNewPageToLinks(int id, AddNewPageToLinks command)
-		{
-			return executeCommand(id, command);
-		}
-
-		private HttpResponseMessage executeCommand(int id, IPageCommand command)
-		{
-			if (command == null)
-			{
-				throw new DomainValidationException("Invalid command.");
-			}
-
-			command.PageID = id;
-			_commandService.Execute(command);
-			return Get(id);
-		}
-
-
-		//[HttpPost, Http.PagePermit(Permissions.Read)]
-		//public HttpResponseMessage ExecuteCommand(int id, string commandName, object command)
-		//{
-		//	// make sure it's josn
-		//	var jsonCommand = command as JObject;
-		//	if (jsonCommand == null)
-		//	{
-		//		return Request.CreateBadRequestResponse("Invalid media type.");
-		//	}
-
-		//	// make sure it's an actual command
-		//	var commandType = _commandService.GetCommandType(commandName);
-		//	if (commandType == null)
-		//	{
-		//		return Request.CreateBadRequestResponse("Invalid command.");
-		//	}
-
-		//	// can we convert it
-		//	var commandToExecute = jsonCommand.ToObject(commandType) as IPageCommand;
-		//	if (commandToExecute == null)
-		//	{
-		//		throw new Exception("Command cannot be null.");
-		//	}
-
-		//	// execute the command
-		//	commandToExecute.PageID = id;
-		//	_commandService.Execute(commandToExecute);
-
-		//	return Get(id);
-		//}
-		#endregion
 	}
 }
