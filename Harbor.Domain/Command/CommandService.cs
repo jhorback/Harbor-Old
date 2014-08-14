813b60f3706628e84959676a1de0e6eaa7664e49
John Horback <jhorback@gmail.com>
2014-08-14 12:28:19 -0400

    Refactoring for base command implementation

---
+++ Harbor.Domain/Command/CommandService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Harbor.Domain
+{
+	public abstract class CommandService<T> where T : ICommand
+	{
+		private readonly CommandContainer commandContainer;
+
+		public CommandService(ICommandContainerRepository commandContainerRepository, Type handlerType, Type genericHandlerType)
+		{
+			commandContainer = commandContainerRepository.GetCommandContainer(handlerType, genericHandlerType);
+		}
+
+		public void Execute(T command)
+		{
+			commandContainer.Execute(command);
+		}
+	}
+}
---
+++ Harbor.Domain/Command/ICommandContainerRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Harbor.Domain
+{
+	public interface ICommandContainerRepository
+	{
+		CommandContainer GetCommandContainer(Type handlerType, Type genericHandlerType);
+	}
+}
