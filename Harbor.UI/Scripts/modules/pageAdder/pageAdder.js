44b31c2b4f35998f4cf3c6ed18f68b1d0a74f893
John Horback <jhorback@gmail.com>
2014-08-15 10:36:35 -0400

    Updated the pageAdder so it can be used by the links component

--- Harbor.UI/Scripts/modules/pageAdder/pageAdder.js
+++ Harbor.UI/Scripts/modules/pageAdder/pageAdder.js
@@ -1,4 +1,13 @@
 ﻿
+/*
+pageAdder options : {
+	addPage: function (page) { }
+}
+
+If defining the addPage method, this component will simply pass the page model.
+It will not add the page or even close the dialog.
+There will also be server validation on the page name.
+*/
 pageAdder.component("pageAdder", {
 	el: "#dialog"
 });
