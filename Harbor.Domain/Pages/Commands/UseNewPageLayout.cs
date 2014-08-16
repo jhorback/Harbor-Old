4bf71288636909f932356c65456d2d8efd0487d8
John Horback <jhorback@gmail.com>
2014-08-15 20:52:00 -0400

    Implemented the use new page layout command

---
+++ Harbor.Domain/Pages/Commands/UseNewPageLayout.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Harbor.Domain.Pages.Commands
+{
+	public class UseNewPageLayout : IPageCommand
+	{
+		public int PageID { get; set; }
+	}
+
+	public class UseNewPageLayoutHandler : IPageCommandHandler<UseNewPageLayout>
+	{
+		private readonly IPageRepository _pageRepository;
+
+		public UseNewPageLayoutHandler(IPageRepository pageRepository)
+		{
+			_pageRepository = pageRepository;
+		}
+
+		public void Execute(UseNewPageLayout command)
+		{
+			var page = _pageRepository.FindById(command.PageID, readOnly: false);
+
+			page.PageLayoutID = 0;
+			page.Layout = new PageLayout
+			{
+				Title = page.Title,
+				UserName = page.AuthorsUserName
+			};
+
+			_pageRepository.Update(page);
+			_pageRepository.Save();
+		}
+	}
+}
