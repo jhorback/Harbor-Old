372cd2a282e7b442a36a683f5848757f7817e135
John Horback <jhorback@gmail.com>
2014-08-16 21:09:32 -0400

    Pulled the command handler into bbext

---
+++ Harbor.UI/Scripts/bbext/commandHandler.js
@@ -0,0 +1,14 @@
+﻿
+
+bbext.commandHandler = function (ajaxRequest) {
+	return {
+		execute: function (model, commandName, command, handler, context) {
+			ajaxRequest.handle(model.postCommand(commandName, command), handler, context);
+		}
+	};
+};
+
+bbext.service("commandHandler", [
+	"ajaxRequest",
+	bbext.commandHandler
+]);
