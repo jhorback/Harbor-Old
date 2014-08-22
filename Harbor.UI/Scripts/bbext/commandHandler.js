09fb739cf214910edd39ab28e65c2a5bda405426
John Horback <jhorback@gmail.com>
2014-08-22 15:17:46 -0400

    Updated the command handler to set the model properties when the results come back from the server.

--- Harbor.UI/Scripts/bbext/commandHandler.js
+++ Harbor.UI/Scripts/bbext/commandHandler.js
@@ -3,7 +3,14 @@
 bbext.commandHandler = function (ajaxRequest) {
 	return {
 		execute: function (model, commandName, command, handler, context) {
-			ajaxRequest.handle(model.postCommand(commandName, command), handler, context);
+			var xhr = ajaxRequest.handle(model.postCommand(commandName, command), handler, context);
+
+			xhr.then(function (resp) {
+				var serverAttrs = model.parse(resp, {});
+				model.set(serverAttrs);
+			});
+
+			return xhr;
 		}
 	};
 };
