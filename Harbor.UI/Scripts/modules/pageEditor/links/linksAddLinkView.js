12713f9285246968cf5846d9ae5a72735516e8d1
John Horback <jhorback@gmail.com>
2014-08-22 15:53:48 -0400

    Working on the usability of adding links

--- Harbor.UI/Scripts/modules/pageEditor/links/linksAddLinkView.js
+++ Harbor.UI/Scripts/modules/pageEditor/links/linksAddLinkView.js
@@ -1,35 +1,40 @@
 ﻿
 
-pageEditor.linksAddLinkView = function (options, pageSelector, pageAdder, dialogFactory, commandHandler) {
-
+pageEditor.linksAddLinkView = function (
+	options, 
+	pageSelector, 
+	pageAdder, 
+	dialogFactory, 
+	commandHandler, 
+	feedback
+) {
 	this.pageSelector = pageSelector;
 	this.pageAdder = pageAdder;
 	this.dialogFactory = dialogFactory;
 	this.commandHandler = commandHandler;
+	this.feedback = feedback;
 };
 
 pageEditor.linksAddLinkView.prototype = {
 	initialize: function () {
 		this.bindAll("addNewPage", "addExistingPage", "addPage", "selectPage");
-		// this.model.collection.page -> page
 	},
 
 	onRender: function () {
 		this.dialog = this.dialogFactory.create(this.$el, {
-			title: "Add a link",
-			modal: false
+			title: "Add a link"
 		});
 	},
 
 	addNewPage: function () {
-		//this.close();
+		this.close();
 		this.pageAdder.render({
 			addPage: this.addPage
 		});
 	},
 
 	addExistingPage: function () {
-		//this.close();
+		this.close();
 		this.pageSelector.render({
 			select: this.selectPage
 		});
@@ -37,9 +42,11 @@ pageEditor.linksAddLinkView.prototype = {
 
 	addPage: function (pageToAdd) {
 
-		var thisPage = this.model.collection.page;
+		var thisPage = this.model.collection.page,
+		    wait = this.feedback.wait("Adding page...");
 
 		this.pageAdder.close();
+
 		this.commandHandler.execute(thisPage, "addNewPageToLinks", {
 			title: pageToAdd.get("title"),
 			pageType: pageToAdd.get("pageTypeKey"),
@@ -47,17 +54,21 @@ pageEditor.linksAddLinkView.prototype = {
 		}, {
 			// handler
 			clientError: function (error) {
-				this.displayErrors(error.errors);
-			}
+				wait.finished(error.errors[""].toString());
+				alert(error.errors[""].toString());
+			},
+			success: wait.finishedMessage()
 		}, this);
 	},
 
 	selectPage: function (selectedPage) {
-		var thisPage = this.model.collection.page;
+		var thisPage = this.model.collection.page,
+			wait = this.feedback.wait("Adding page...");
+
 		this.commandHandler.execute(thisPage, "addExistingPageToLinks", {
 			existingPageID: selectedPage.id,
 			sectionIndex: this.model.collection.indexOf(this.model)
-		}, this);
+		}, this).then(wait.finishedMessage());
 	},
 
 	onClose: function () {
@@ -72,6 +83,7 @@ pageEditor.view("linksAddLinkView", [
 	"pageAdder",
 	"dialogFactory",
 	"commandHandler",
+	"feedback",
 	pageEditor.linksAddLinkView
 ]);
 
