891cf322fde792dc2be84780b753718fd1b04bd6
John Horback <jhorback@gmail.com>
2014-08-23 12:06:00 -0400

    Added initial unit test for the new page type methods

---
+++ Harbor.Domain.Tests/PageTypeRepositoryTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Harbor.Domain.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Harbor.Domain.Tests
+{
+	[TestClass]
+	public class PageTypeRepositoryTest
+	{
+		private TestContext testContextInstance;
+
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+
+		[TestMethod]
+		public void GetPageTypesToAdd_IsNotEmpty()
+		{
+			var repo = new PageTypeRepository();
+
+			var types = repo.GetPageTypesToAdd();
+
+			Assert.IsTrue(types.Any());
+			Assert.IsTrue(types["primary"].Any());
+			Assert.IsTrue(types["other"].Any());
+		}
+
+		// jch! - add more tests for the sub page types
+	}
+}
