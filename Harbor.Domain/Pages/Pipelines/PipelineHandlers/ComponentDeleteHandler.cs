73e9dfd13cd4846af3ef38591cd415b23788e1dc
John Horback <jhorback@gmail.com>
2014-08-24 12:24:28 -0400

    Added initial delete handlers and implemented the delete layout handler

---
+++ Harbor.Domain/Pages/Pipelines/PipelineHandlers/ComponentDeleteHandler.cs
@@ -0,0 +1,16 @@
+﻿using Harbor.Domain.Pipeline;
+
+namespace Harbor.Domain.Pages.PipelineHandlers
+{
+	public class ComponentDeleteHandler : IPipelineHanlder<Page>
+	{
+
+		public void Execute(Page context)
+		{
+			// jch! - need to implement
+			// load the component repository (or whatever) - need OnDelete method
+			// would looop through the template content
+			// call OnDelete for each component
+		}
+	}
+}
---
+++ Harbor.Domain/Pages/Pipelines/PipelineHandlers/DeleteLayoutDeleteHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Harbor.Domain.Pipeline;
+
+namespace Harbor.Domain.Pages.PipelineHandlers
+{
+	/// <summary>
+	/// Deletes the layout if the page being deleted is the last one that uses it.va
+	/// </summary>
+	public class DeleteLayoutDeleteHandler : IPipelineHanlder<Page>
+	{
+		private readonly IPageLayoutRepository _pageLayoutRepository;
+
+		public DeleteLayoutDeleteHandler(IPageLayoutRepository pageLayoutRepository)
+		{
+			_pageLayoutRepository = pageLayoutRepository;
+		}
+
+		public void Execute(Page page)
+		{
+			var layoutCount = _pageLayoutRepository.Query().Count(l => l.PageLayoutID == page.PageLayoutID);
+			if (layoutCount == 1)
+			{
+				var layout = _pageLayoutRepository.FindById(page.PageLayoutID);
+				_pageLayoutRepository.Delete(layout);
+			}
+		}
+	}
+}
