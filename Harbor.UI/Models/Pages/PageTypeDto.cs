56922cd403eb9e05ffc528d3a2946b0ff0879e40
John Horback <jhorback@gmail.com>
2014-08-27 22:34:26 -0400

    Determined the pate type description on the server

--- Harbor.UI/Models/Pages/PageTypeDto.cs
+++ Harbor.UI/Models/Pages/PageTypeDto.cs
@@ -7,21 +7,19 @@ namespace Harbor.UI.Models
 		public string key { get; set; }
 		public string name { get; set; }
 		public string description { get; set; }
-		public string contentDescription { get; set; }
 		public bool isPrimaryToAdd { get; set; }
 
-		public PageTypeDto(PageType pageType, bool isPrimary)
+		public PageTypeDto(PageType pageType, bool isPrimary, bool addingToParent)
 		{
 			key = pageType.Key;
 			name = pageType.Name;
-			description = pageType.Description;
-			contentDescription = pageType.ContentDescription;
+			description = addingToParent ? pageType.ContentDescription : pageType.Description;
 			isPrimaryToAdd = isPrimary;
 		}
 
-		public static PageTypeDto FromPageType(PageType pageType, bool isPrimaryToAdd)
+		public static PageTypeDto FromPageType(PageType pageType, bool isPrimaryToAdd, bool addingToParent)
 		{
-			return new PageTypeDto(pageType, isPrimaryToAdd);
+			return new PageTypeDto(pageType, isPrimaryToAdd, addingToParent);
 		}
 	}
 }
