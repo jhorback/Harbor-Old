c1e2d3de76c20914b504267c9d75bc46a1488177
John Horback <jhorback@gmail.com>
2014-08-30 14:04:20 -0400

    Added the dto mapper

---
+++ Harbor.UI/Models/Shared/MapDtoFromAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Harbor.UI.Models
+{
+	/// <summary>
+	/// Use with IDtoMapper.
+	/// The class that applies this attribute must have a contructor that
+	/// takes the FromType as a single parameter.
+	/// </summary>
+	[AttributeUsage(validOn: AttributeTargets.Class, AllowMultiple = false)]
+	public class MapDtoFromAttribute : Attribute
+	{
+		public MapDtoFromAttribute(Type type)
+		{
+			FromType = type;
+		}
+
+		/// <summary>
+		/// The dto must define a constructor with the FromType as a single parameter.
+		/// </summary>
+		public Type FromType
+		{
+			get;
+			set;
+		}
+	}
+}
