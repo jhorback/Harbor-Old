42de42109861134e1bdf222f1e43f70c3de24fb3
John Horback <jhorback@gmail.com>
2014-09-07 10:36:59 -0400

    Added the add template content command

---
+++ Harbor.Domain/Pages/Commands/AddTemplateContent.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Harbor.Domain.Pages.Commands
+{
+	public class AddTemplateContent : IPageCommand
+	{
+		public AddTemplateContent() 
+		{
+			Prepend = false;
+		}
+
+		public int PageID { get; set; }
+		public string ContentKey { get; set; }
+		public bool Prepend { get; set; }
+	}
+
+	public class AddTemplateContentHandler : IPageCommandHandler<AddTemplateContent>
+	{
+		private readonly IPageRepository _pageRepository;
+		
+
+		public AddTemplateContentHandler(IPageRepository pageRepository)
+		{
+			_pageRepository = pageRepository;
+		}
+
+		public void Execute(AddTemplateContent command)
+		{
+			var page = _pageRepository.FindById(command.PageID, readOnly: false);
+			if (command.Prepend)
+			{
+				page.Template.PrependContent(command.ContentKey);
+			}
+			else
+			{
+				page.Template.AppendContent(command.ContentKey);				
+			}
+			_pageRepository.Update(page);
+			_pageRepository.Save();
+		}
+	}
+}
