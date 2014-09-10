23b56998833db8de1698fb833392598cd859ab63
John Horback <jhorback@gmail.com>
2014-09-10 15:47:15 -0400

    Added an "exists" method for template content and made sure the key comparison was case insensitive

--- Harbor.Domain/Pages/IContentTypeRespository.cs
+++ Harbor.Domain/Pages/IContentTypeRespository.cs
@@ -5,6 +5,7 @@ namespace Harbor.Domain.Pages
 	public interface IContentTypeRepository
 	{
 		IEnumerable<TemplateContentType> GetTemplateContentTypes();
+		bool TemplateContentTypeExists(string key);
 		TemplateContentHandler GetTemplateContentHandler(TemplateUic uic, Page page);
 		PageLayoutContentHandler GetLayoutContentHandler(string key, Page page);
 	}
