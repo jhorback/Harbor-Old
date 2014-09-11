0cff9565698aca864a572d51c1c5a4b620e8de14
John Horback <jhorback@gmail.com>
2014-09-11 17:14:25 -0400

    Added the delete template content command

---
+++ Harbor.Domain/Pages/Commands/DeleteTemplateContent.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+
+namespace Harbor.Domain.Pages.Commands
+{
+	public class DeleteTemplateContent : IPageCommand
+	{
+		public int PageID { get; set; }
+		public string UicId { get; set; }
+	}
+
+	public class DeleteTemplateContentHandler : IPageCommandHandler<DeleteTemplateContent>
+	{
+		private readonly IPageRepository _pageRepository;
+		private readonly IContentTypeRepository _contentTypeRepository;
+
+
+		public DeleteTemplateContentHandler(IPageRepository pageRepository, IContentTypeRepository contentTypeRepository)
+		{
+			_pageRepository = pageRepository;
+			_contentTypeRepository = contentTypeRepository;
+		}
+
+		public void Execute(DeleteTemplateContent command)
+		{
+			var page = _pageRepository.FindById(command.PageID, readOnly: false);
+
+			var uic = page.Template.Content.FirstOrDefault(c => c.Id == command.UicId);
+			if (uic == null)
+			{
+				return;
+			}
+
+			var handler = _contentTypeRepository.GetTemplateContentHandler(uic, page);
+			if (handler != null)
+			{
+				handler.OnDelete();
+			}
+
+			page.Template.Content.Remove(uic);
+
+			_pageRepository.Update(page);
+			_pageRepository.Save();
+		}
+	}
+}
