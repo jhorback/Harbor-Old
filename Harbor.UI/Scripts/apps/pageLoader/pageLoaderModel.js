2e9638302f9fcabfeda7a4fe6df9c65d83b7893b
John Horback <jhorback@gmail.com>
2014-09-28 13:38:34 -0400

    Made sure the pageEditor is open when settings pane is visible

--- Harbor.UI/Scripts/apps/pageLoader/pageLoaderModel.js
+++ Harbor.UI/Scripts/apps/pageLoader/pageLoaderModel.js
@@ -1,24 +1,8 @@
 ﻿
 var pageLoaderModel = {
 	defaults: {
-		mode: "view",
-		doneButtonVisible: false
-	},
-
-	doneButtonVisible: {
-		get: function (value) {
-			var ret;
-			if (this.get("mode") === "view") {
-				ret = false;
-				// not sure why I have to do this
-				$("[data-bind='showif: doneButtonVisible']").hide();
-			} else {
-				ret = true;
-			}
-			// console.debug("doneButtonVisible", ret);
-			return ret;
-		},
-		bind: ["mode"]
+		editingPage: false,
+		settingsOpen: false
 	}
 };
 
--- Harbor.UI/Scripts/apps/pageLoader/pageLoaderView.js
+++ Harbor.UI/Scripts/apps/pageLoader/pageLoaderView.js
@@ -9,21 +9,35 @@ function pageLoaderView(options, modelFactory, pageEditor, pageSettings) {
 
 pageLoaderView.prototype = {
 	initialize: function () {
+		this.bindAll("onSettingsClose");
 		this.model = this.modelFactory.create("pageLoaderModel");
 	},
 
 	showSettings: function (event) {
-		this.pageSettings.render();
+		this.model.set("settingsOpen", true);
+		if (this.model.attributes.editingPage === false) {
+			this.pageEditor.render();
+		}
+		this.settingsView = this.pageSettings.render();
+		this.settingsView.on("close", this.onSettingsClose);
+	},
+
+	onSettingsClose: function () {
+		this.settingsView && this.settingsView.off("close", this.onSettingsClose);
+		this.model.set("settingsOpen", false);
+		if (this.model.attributes.editingPage === false) {
+			this.doneEditing();
+		}
 	},
 
 	editPage: function (event) {
-		this.model.set("mode", "edit");
+		this.model.set("editingPage", true);
 		this.pageEditor.render();
 	},
 
 	doneEditing: function (event) {
 		this.pageEditor.close();
-		this.model.set("mode", "view");
+		this.model.set("editingPage", false);
 	}
 };
 
