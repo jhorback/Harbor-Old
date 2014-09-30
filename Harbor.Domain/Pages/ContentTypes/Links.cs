3d26dd86cf0a555741c80727303b71d2f77ae538
John Horback <jhorback@gmail.com>
2014-09-30 16:12:39 -0400

    Added a query to sync page names in the nav links

--- Harbor.Domain/Pages/ContentTypes/Links.cs
+++ Harbor.Domain/Pages/ContentTypes/Links.cs
@@ -15,10 +15,12 @@ namespace Harbor.Domain.Pages.ContentTypes
 	public class LinksHandler : PageLayoutContentHandler
 	{
 		private readonly ILogger _logger;
+		private readonly IPageRepository _pageRepository;
 
-		public LinksHandler(ILogger logger, Page page) : base(page)
+		public LinksHandler(ILogger logger, Page page, IPageRepository pageRepository) : base(page)
 		{
 			_logger = logger;
+			_pageRepository = pageRepository;
 		}
 
 		public override void OnDelete()
@@ -62,7 +64,26 @@ namespace Harbor.Domain.Pages.ContentTypes
 		{
 			var aside = GetAside<Content.Links>() ?? new Content.Links();
 			aside.EnsureIds();
+			syncPageNames(aside);
 			return aside;
+		}
+
+		// does a query to sync the page names
+		void syncPageNames(Content.Links links)
+		{
+			var pageIDs = links.sections.SelectMany(s => s.links).Select(l => l.pageID).Distinct().ToArray();
+			var pages = _pageRepository.Query().Where(p => pageIDs.Contains(p.PageID)).ToDictionary(p => p.PageID);
+
+			foreach (var section in links.sections)
+			{
+				foreach (var link in section.links)
+				{
+					if (pages.ContainsKey(link.pageID))
+					{
+						link.text = pages[link.pageID].Title;
+					}
+				}
+			}
 		}
 	}
 }
