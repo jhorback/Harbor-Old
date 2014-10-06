e3ab557e6d0dfcbd7d9c0a16709010290cce0df0
John Horback <jhorback@gmail.com>
2014-10-06 16:16:44 -0400

    Added the root pages domain objects

---
+++ Harbor.Domain/App/RootPages.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Harbor.Domain.App
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class RootPages
+	{
+		public Dictionary<int, string> Pages { get; set; }
+	}
+}
---
+++ Harbor.Domain/App/RootPagesRepository.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Harbor.Domain.App
+{
+	public class RootPagesRepository
+	{
+		private readonly IAppSettingRepository _appSettingRepository;
+
+		public RootPagesRepository(IAppSettingRepository appSettingRepository)
+		{
+			_appSettingRepository = appSettingRepository;
+		}
+
+		public RootPages GetRootPages()
+		{
+			RootPages pages = null;
+			var appSetting = _appSettingRepository.FindByName("RootPages");
+			if (appSetting != null)
+			{
+				pages = JSON.Parse<RootPages>(appSetting.Value);
+			}
+
+			return pages ?? (pages = new RootPages());
+		}
+
+		public void RemoveRootPage(int id)
+		{
+			var pages = GetRootPages();
+			pages.Pages.Remove(id);
+			saveAppSetting(pages);
+
+		}
+
+		public void AddRootPage(int id, string text)
+		{
+			var pages = GetRootPages();
+			pages.Pages.Add(id, text);
+			saveAppSetting(pages);
+		}
+
+		public void Save()
+		{
+			_appSettingRepository.Save();
+		}
+
+		private void saveAppSetting(RootPages pages)
+		{
+			var appSetting = _appSettingRepository.FindByName("RootPages", readOnly: false);
+			var settingValue = JSON.Stringify(pages);
+			if (appSetting == null)
+			{
+				appSetting = _appSettingRepository.Create(new AppSetting {Name = "RootPages", Value = settingValue});
+			}
+			else
+			{
+				if (settingValue != appSetting.Value)
+				{
+					appSetting.Value = settingValue;
+					_appSettingRepository.Update(appSetting);
+				}
+			}
+		}
+	}
+
+
+	public interface IRootPagesRepository
+	{
+		RootPages GetRootPages();
+		void RemoveRootPage(int id);
+		void AddRootPage(int id, string text);
+		void Save();
+	}
+}
