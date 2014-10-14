6655998eeb4f71457bdbd0c8d4b88f6a9e7b35c7
John Horback <jhorback@gmail.com>
2014-10-14 14:43:28 -0400

    Added documentation to the command handler

--- Harbor.UI/Scripts/bbext/commandHandler.js
+++ Harbor.UI/Scripts/bbext/commandHandler.js
@@ -1,5 +1,14 @@
-﻿
-
+﻿/*
+ * Enables command execution on a model.
+ * 
+ * commandHandler.execute(model, commandName, [command], [handler], [context])
+ *     model - the model to execute the command on. Uses the model.executeCommand model extension method.
+ *     commandName - the command name to execute. This is appended to the models url to determine the commands url.
+ *     command - on object to send as data which represents the command object
+ *     handler - an object passed to the ajaxRequest to handle specific success, failure, or http condes.
+ *     context - a binding context to be passed to the handler callback methods.
+ *
+ */
 bbext.commandHandler = function (ajaxRequest) {
 	return {
 		execute: function (model, commandName, command, handler, context) {
