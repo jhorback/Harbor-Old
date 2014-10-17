c7d41dab92359025e9597049c762066b0d69b128
John Horback <jhorback@gmail.com>
2014-10-17 17:30:33 -0400

    Added the artwork page type

---
+++ Harbor.Domain/Pages/PageTypes/Artwork.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Harbor.Domain.Pages.PageTypes
+{
+	public class Artwork : PageType
+	{
+		public override string Key
+		{
+			get { return "artwork"; }
+		}
+
+		public override string Name
+		{
+			get { return "Artwork"; }
+		}
+
+		public override string Description
+		{
+			get { return "A page with image and text."; }
+		}
+
+		public override string ContentDescription
+		{
+			get { return "Contains image and text."; }
+		}
+		
+
+		public override void SetLayout(PageTypeLayoutContext context)
+		{
+			context
+				.SetLayout(PageLayout.LayoutDisplayProperties.ContentCentered | PageLayout.LayoutDisplayProperties.NoAside)
+				.SetHeader(LayoutContentTypes.Title);
+		}
+
+		public override void SetTemplate(PageTypeTemplateContext context)
+		{
+			context
+				.AddContent(TemplateContentTypes.Image)
+				.AddContent(TemplateContentTypes.Text);
+		}
+
+		public override void SetAddPageTypeFilter(AddPageTypeFilterContext context)
+		{
+			context.IsPrimary(false);
+		}
+
+		public override void SetAddContentTypeFilter(AddContentTypeFilterContext context)
+		{
+			context
+				.SuggestContentType<ContentTypes.Image>()
+				.SuggestContentType<ContentTypes.Text>();
+		}
+	}
+}
