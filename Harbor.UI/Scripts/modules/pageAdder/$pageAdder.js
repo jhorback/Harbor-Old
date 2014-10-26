45cb1f6df7bc4de9e948f47e381ad94126e9bcb9
John Horback <jhorback@gmail.com>
2014-10-26 11:35:16 -0400

    Removed a redundant pageAdder file

--- Harbor.UI/Scripts/modules/pageAdder/$pageAdder.js
+++ Harbor.UI/Scripts/modules/pageAdder/$pageAdder.js
@@ -1,3 +1,12 @@
-﻿
+﻿/*
+pageAdder options
+	parentPageTypeKey - An optional page type used to filter and adjust the page type selection.
+	onAddPage - a callback recieving the selected page as an argument.
+	createPage - if false, the page save is not called
+*/
+
 var pageAdder = context.module("pageAdder").use("bbext", "currentUserModel", "pageModel");
 
+pageAdder.component("pageAdder", {
+	el: "#dialog"
+});
