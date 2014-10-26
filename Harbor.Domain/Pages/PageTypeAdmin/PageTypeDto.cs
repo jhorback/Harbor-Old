e51e7fb1e72df62b42c36f7d1e252e3102bed35f
John Horback <jhorback@gmail.com>
2014-10-26 13:51:42 -0400

    Created the page type query

---
+++ Harbor.Domain/Pages/PageTypeAdmin/PageTypeDto.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Harbor.Domain.Pages.PageTypeAdmin
+{
+	public class PageTypeDto
+	{
+		public string key { get; set; }
+		public string name { get; set; }
+		public string description { get; set; }
+		public string contentDescription { get; set; }
+		public string addContentFilterDescription { get; set; }
+		public string addPageFilterDescription { get; set; }
+		public bool isPrimaryToAdd { get; set; }
+
+		// the query does the mapping
+		//public PageTypeDto(IPageType pageType, bool isPrimary, bool addingToParent)
+		//{
+		//	key = pageType.Key;
+		//	name = pageType.Name;
+		//	description = pageType.Description;
+		//	contentDescription = pageType.ContentDescription;
+		//	isPrimaryToAdd = isPrimary;
+
+		//	// jch! - create a query for page types. SystemPageTypesQuery - returns all page types
+		//	/* NEED TO SET THE addContentFilter and the addPageFilterDescription
+		//	 * */
+		//}
+
+		//public static PageTypeDto FromPageType(IPageType pageType, bool isPrimaryToAdd, bool addingToParent)
+		//{
+		//	return new PageTypeDto(pageType, isPrimaryToAdd, addingToParent);
+		//}
+	}
+}
---
+++ Harbor.Domain/Pages/PageTypeAdmin/Queries/PageTypeQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Harbor.Domain.Query;
+
+namespace Harbor.Domain.Pages.PageTypeAdmin.Queries
+{
+	public interface IPageTypeQuery : ICachedQuery<IEnumerable<PageTypeDto>>
+	{
+	
+	}
+
+
+	public class PageTypeQuery : CachedQueryBase<IEnumerable<PageTypeDto>>, IPageTypeQuery
+	{
+		private readonly IPageTypeRepository _pageTypeRepository;
+
+		public PageTypeQuery(IPageTypeRepository pageTypeRepository)
+		{
+			_pageTypeRepository = pageTypeRepository;
+		}
+
+		public override IEnumerable<PageTypeDto> Execute()
+		{
+			
+		}
+	}
+}
