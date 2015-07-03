3e1e33a7c62822bf3156fb937e873ca5628a1129
John Horback <jhorback@gmail.com>
2015-07-03 10:29:31 -0400

    Updated the version to 10

--- Harbor.Domain/Properties/AssemblyInfo.cs
+++ Harbor.Domain/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.5.2.*")]
+[assembly: AssemblyVersion("10.0.0.*")]
 
 
 // Setup InternalsVisibleTo
