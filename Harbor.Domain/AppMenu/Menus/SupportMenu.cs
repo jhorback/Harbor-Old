147253436bd43535824816de1011bad4acf36bac
John Horback <jhorback@gmail.com>
2015-07-07 18:41:24 -0400

    Added the support menu

---
+++ Harbor.Domain/AppMenu/Menus/SupportMenu.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using Harbor.Domain.App;
+using Harbor.Domain.Security;
+
+namespace Harbor.Domain.AppMenu.Menus
+{
+    public class SupportMenu : Menu
+    {
+		public override List<MenuItem> Items
+		{
+			get
+			{
+				return new List<MenuItem>
+				{
+					new StyleGuideMenuLink(),
+					new JavaScriptPackagesMenuLink(),
+					new ElmahMenuLink(),
+					new AnalyticsMenuLink()
+				};
+			}
+		}
+
+		public override string Id
+		{
+			get { return "support"; }
+		}
+
+		public override string Text
+		{
+			get { return "Support"; }
+		}
+
+	    public override bool HasPermission(MenuItemContext context)
+	    {
+		    return context.User.HasPermission(UserFeature.SystemSettings);
+	    }
+    }
+
+	public class StyleGuideMenuLink : MenuLink
+	{
+		public override string Url
+		{
+			get { return "~/styleguide"; }
+		}
+
+		public override string Id
+		{
+			get { return "style-guide"; }
+		}
+
+		public override string Text
+		{
+			get { return "Style Guide"; }
+		}
+	}
+
+	public class JavaScriptPackagesMenuLink : MenuLink
+	{
+		public override string Url
+		{
+			get { return "~/home/javascriptpackages"; }
+		}
+
+		public override string Id
+		{
+			get { return "javascript-packages"; }
+		}
+
+		public override string Text
+		{
+			get { return "JavaScript Packages"; }
+		}
+	}
+
+	public class ElmahMenuLink : MenuLink
+	{
+		public override string Url
+		{
+			get { return "~/elmah.axd"; }
+		}
+
+		public override string Id
+		{
+			get { return "elmah"; }
+		}
+
+		public override string Text
+		{
+			get { return "Elmah (http errors)"; }
+		}
+	}
+
+	public class AnalyticsMenuLink : DynamicMenuLink
+	{
+		public override string Url
+		{
+			get { return "https://www.google.com/analytics"; }
+		}
+
+		public override string Id
+		{
+			get { return "analytics"; }
+		}
+
+		public override string GetText(MenuItemContext context)
+		{
+			var harborApp = context.GetDependency<IHarborAppRepository>().GetApp();
+			if (string.IsNullOrEmpty(harborApp.GoogleAnalyticsAccount))
+			{
+				return "Google Analytics (not configured)";
+			}
+
+			return "Google Analytics " + harborApp.GoogleAnalyticsAccount;
+		}
+	}
+}
