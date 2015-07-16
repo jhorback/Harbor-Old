454cd846307631986b0b9632aa10dbfb8961bd22
John Horback <jhorback@gmail.com>
2015-07-16 02:20:47 -0400

    Working on converting the style guide

--- Harbor.UI/Controllers/StyleGuideController.cs
+++ Harbor.UI/Controllers/StyleGuideController.cs
@@ -1,51 +1,74 @@
 ﻿using System.Web.Mvc;
+using System.Web.WebPages;
+using Harbor.Domain.AppMenu.Queries;
+using Harbor.Domain.Query;
 using Harbor.Domain.Security;
-using Harbor.UI.Models.StyleGuide;
+
+
 
 namespace Harbor.UI.Controllers
 {
 	[Permit(UserFeature.SystemSettings, Permissions.Read), RoutePrefix("styleguide")]
 	public class StyleGuideController : Controller
 	{
-		readonly StyleGuidePageRepository repository;
+		private readonly IQueryService _queryService;
 
-		public StyleGuideController(StyleGuidePageRepository repository)
+		public StyleGuideController(IQueryService queryService)
 		{
-			this.repository = repository;
+			_queryService = queryService;
 		}
 
-		[Route("{pageKey=home}", Name = "StyleGuidePage")]
-		public ActionResult Index(string pageKey)
-		{
-			var page = repository.GetPage(pageKey);
-			if (page == null || page.Key == "Home")
-			{
-				if (pageKey == null)
-				{
-					pageKey = "Home";
-					ViewBag.Title = "Style Guide";
-				}
-				else ViewBag.Title = pageKey;
-			}
-			else
-			{
-				ViewBag.Title = page.Name + " - Style Guide";
-			}
-			var pages = repository.GetPages();
-			return View(pageKey, pages);
-		}
+		//[Route("{pageKey=home}", Name = "StyleGuidePage")]
+		//public ActionResult Index(string pageKey)
+		//{
+		//	var page = repository.GetPage(pageKey);
+		//	if (page == null || page.Key == "Home")
+		//	{
+		//		if (pageKey == null)
+		//		{
+		//			pageKey = "Home";
+		//			ViewBag.Title = "Style Guide";
+		//		}
+		//		else ViewBag.Title = pageKey;
+		//	}
+		//	else
+		//	{
+		//		ViewBag.Title = page.Name + " - Style Guide";
+		//	}
+		//	var pages = repository.GetPages();
+		//	return View(pageKey, pages);
+		//}
 
-		[Route("PageNav")]
-		public PartialViewResult PageNav()
-		{
-			var pages = repository.GetPages();
-			return PartialView("_PageNav", pages);
-		}
-
+		// MOVE THIS TO STAGING
 		[Route("TestApp")]
 		public ViewResult TestApp()
 		{
 			return View("TestApp");
+		}
+
+
+
+
+
+		[Route("{menuId}", Name = "NewStyleGuidePage")]
+		public ActionResult ShowStyleGuidePage(string menuId)
+		{
+			var url = Request.Url.ToString().ToLower();
+			var title = "";
+
+			var menuItems = _queryService.GetQuery<MenuQuery>().ExecuteFromCache();
+			foreach (var item in menuItems)
+			{
+				if (item.url != null && url.IndexOf(item.url.ToLower()) > -1)
+				{
+					title = item.text;
+					break;
+				}
+			}
+
+			ViewBag.Title = title;
+
+			return View(menuId);
 		}
 	}
 }
