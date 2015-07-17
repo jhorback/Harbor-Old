d241ce55105b3e81b2fe97cb38066c8a12ccbfe4
John Horback <jhorback@gmail.com>
2015-07-17 16:07:22 -0400

    Added the menu changed event and subscriber

---
+++ Harbor.Domain/AppMenu/Events/MenuChangedEvent.cs
@@ -0,0 +1,11 @@
+﻿using Harbor.Domain.Event;
+
+namespace Harbor.Domain.AppMenu.Events
+{
+	/// <summary>
+	/// Trigger this event to bust the menu cache.
+	/// </summary>
+	public class MenuChangedEvent : IEvent
+	{
+	}
+}
