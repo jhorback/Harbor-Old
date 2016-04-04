e83b494bd779920e297157f3755f97c591c47608
John Horback <jhorback@gmail.com>
2016-04-04 00:10:53 -0400

    Updated .gitignore and added the shiv pkg

---
+++ Harbor.UI/App_Start/JSPkgs/Framework/Html5ShivPkg.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Optimization;
+using Harbor.UI.Models.JSPM;
+
+namespace Harbor.UI.JSPkgs
+{
+	public class Html5ShivPkg : JavaScriptPackage
+	{
+		public const string PackageName = "Html5Shiv";
+
+        public Html5ShivPkg()
+        {
+            Name = PackageName;
+            ScriptBundle = new ScriptBundle("~/scripts/src/html5shiv.min.js")
+                .Include("~/scripts/lib/html5shiv/dist/html5shiv.js")
+                .Include("~/scripts/lib/webcomponentsjs/webcomponents-lite.js")
+                //.Include("~/scripts/lib/dom4/build/dom4.max.js")
+                //.Include("~/Scripts/lib/document-register-element/build/document-register-element.max.js")
+                //.Include("~/Scripts/lib/document-register-element/build/innerHTML.max.js")
+                ;
+            RequiresRegistration = false;
+            Category = Categories.DomAbstraction;
+        }
+    }
+}
